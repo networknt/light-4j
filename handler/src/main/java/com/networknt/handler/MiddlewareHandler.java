33b387163ec5b29c6d54d527e65aebd2988fd3e5
Steve Hu <stevehu@gmail.com>
2017-05-10 09:21:07 -0400

    fixes #60 update docs

--- handler/src/main/java/com/networknt/handler/MiddlewareHandler.java
+++ handler/src/main/java/com/networknt/handler/MiddlewareHandler.java
@@ -18,16 +18,43 @@ package com.networknt.handler;
 import io.undertow.server.HttpHandler;
 
 /**
- * Created by steve on 03/10/16.
+ * A interface for middleware handlers. All middleware handlers must implement this interface
+ * so that the handler can be plugged in to the request/response chain during server startup
+ * with SPI (Service Provider Interface). The entire light-4j framework is a core server that
+ * provides a plugin structure to hookup all sorts of plugins to handler different cross-cutting
+ * concerns.
+ *
+ * The middleware handlers are designed based on chain of responsibility pattern.
+ *
+ * @author Steve Hu
  */
 public interface MiddlewareHandler extends HttpHandler {
 
+    /**
+     * Get the next handler in the chain
+     *
+     * @return HttpHandler
+     */
     HttpHandler getNext();
 
+    /**
+     * Set the next handler in the chain
+     *
+     * @param next HttpHandler
+     * @return MiddlewareHandler
+     */
     MiddlewareHandler setNext(final HttpHandler next);
 
+    /**
+     * Indicate if this handler is enabled or not.
+     *
+     * @return
+     */
     boolean isEnabled();
 
+    /**
+     * Register this handler to the handler registration.
+     */
     void register();
 
 }
