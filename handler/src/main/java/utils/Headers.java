ac2ff6089b701d844dc8d7f9d9f9889ba6ed963e
Sachin Walia <sachinwalia@yahoo.com>
2017-09-27 19:09:31 -0700

    Resolved Issue #3 - Utility interfaces for boilerplate tasks such as query, path variable, header extraction

---
+++ handler/src/main/java/utils/Headers.java
@@ -0,0 +1,28 @@
+package utils;
+
+/**
+ * A utility interface with default implementation for extracting headers from the request.
+ *
+ * @author Bill O'Neil (https://github.com/billoneil)
+ * @author Sachin Walia (https://github.com/sachinwalia2k8)
+ *
+ */
+
+import io.undertow.attribute.RequestHeaderAttribute;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
+
+import java.util.Optional;
+
+public interface Headers {
+
+	default Optional<String> getHeader(HttpServerExchange exchange, HttpString header) {
+		RequestHeaderAttribute reqHeader = new RequestHeaderAttribute(header);
+		return Optional.ofNullable(reqHeader.readAttribute(exchange));
+	}
+
+	default Optional<String> getHeader(HttpServerExchange exchange, String header) {
+		RequestHeaderAttribute reqHeader = new RequestHeaderAttribute(new HttpString(header));
+		return Optional.ofNullable(reqHeader.readAttribute(exchange));
+	}
+}
