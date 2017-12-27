899f27cdeaa6eb004270227ca0302a2f21a0584e
Steve Hu <stevehu@gmail.com>
2017-12-26 23:23:10 -0500

    fixes #147 Add email with attachment

--- email/src/main/java/com/networknt/email/EmailSender.java
+++ email/src/main/java/com/networknt/email/EmailSender.java
@@ -3,9 +3,14 @@ package com.networknt.email;
 import com.networknt.common.SecretConfig;
 import com.networknt.config.Config;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import java.util.Properties;
 
 public class EmailSender {
@@ -38,6 +43,50 @@ public class EmailSender {
         message.setSubject(subject);
 
         message.setContent(content, "text/html");
+
+        // Send message
+        Transport.send(message);
+    }
+
+    public void sendMailWithAttachment (String to, String subject, String content, String filename) throws MessagingException{
+        Properties props = new Properties();
+        props.put("mail.smtp.user", emailConfg.getUser());
+        props.put("mail.smtp.host", emailConfg.getHost());
+        props.put("mail.smtp.port", emailConfg.getPort());
+        props.put("mail.smtp.starttls.enable","true");
+        props.put("mail.smtp.debug", emailConfg.getDebug());
+        props.put("mail.smtp.auth", emailConfg.getAuth());
+
+        SMTPAuthenticator auth = new SMTPAuthenticator(emailConfg.getUser(), secretConfig.getEmailPassword());
+        Session session = Session.getInstance(props, auth);
+
+        MimeMessage message = new MimeMessage(session);
+
+        message.setFrom(new InternetAddress(emailConfg.getUser()));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        message.setSubject(subject);
+
+        // Create the message part
+        BodyPart messageBodyPart = new MimeBodyPart();
+
+        // Now set the actual message
+        messageBodyPart.setText("This is message body");
+
+        // Create a multipar message
+        Multipart multipart = new MimeMultipart();
+
+        // Set text message part
+        multipart.addBodyPart(messageBodyPart);
+
+        // Part two is attachment
+        messageBodyPart = new MimeBodyPart();
+        DataSource source = new FileDataSource(filename);
+        messageBodyPart.setDataHandler(new DataHandler(source));
+        messageBodyPart.setFileName(filename);
+        multipart.addBodyPart(messageBodyPart);
+
+        // Send the complete message parts
+        message.setContent(multipart);
 
         // Send message
         Transport.send(message);
--- email/src/test/java/com/networknt/email/EmailSenderTest.java
+++ email/src/test/java/com/networknt/email/EmailSenderTest.java
@@ -3,6 +3,7 @@ package com.networknt.email;
 import org.junit.Test;
 
 import javax.mail.MessagingException;
+import java.io.File;
 
 public class EmailSenderTest {
     //@Test
@@ -10,6 +11,18 @@ public class EmailSenderTest {
         EmailSender sender = new EmailSender();
         try {
             sender.sendMail("stevehu@gmail.com", "test", "This is a test email");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //@Test
+    public void testEmailWithAttachment() {
+        EmailSender sender = new EmailSender();
+        try {
+            File file = new File("pom.xml");
+            String absolutePath = file.getAbsolutePath();
+            sender.sendMailWithAttachment("stevehu@gmail.com", "with attachment", "This is message body", absolutePath);
         } catch (MessagingException e) {
             e.printStackTrace();
         }
