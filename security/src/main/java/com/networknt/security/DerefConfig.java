cdb0b2a0386e6023e6c45157baf9c209adf88782
Steve Hu <stevehu@gmail.com>
2018-06-21 18:23:34 -0400

    fixes #224 Add a middleware handler to de-reference opaque access token to JWT

---
+++ security/src/main/java/com/networknt/security/DerefConfig.java
@@ -0,0 +1,18 @@
+package com.networknt.security;
+
+/**
+ * The config class that maps to deref.yml
+ *
+ * @author Steve Hu
+ */
+public class DerefConfig {
+    boolean enabled;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
