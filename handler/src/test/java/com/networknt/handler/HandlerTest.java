829c31a1f2fcdd91d5e15e01919d64a3ebd99f72
ddobrin <dan.dpobrin@gmail.com>
2018-08-14 15:10:21 -0400

    Fixes #261 Address graceful server shutdown while encountering an exception during start-up

--- handler/src/test/java/com/networknt/handler/HandlerTest.java
+++ handler/src/test/java/com/networknt/handler/HandlerTest.java
@@ -26,6 +26,7 @@ public class HandlerTest {
 
     @Test
     public void validConfig_init_handlersCreated() {
+    	Handler.init();
         Map<String, List<HttpHandler>> handlers = Handler.handlerListById;
         Assert.assertEquals(1, handlers.get("third").size());
         Assert.assertEquals(2, handlers.get("secondBeforeFirst").size());
