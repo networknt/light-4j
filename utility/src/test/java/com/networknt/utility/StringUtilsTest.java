d5920fa2fc578ee0a3a7e639e6eeb0c72246d059
Steve Hu <stevehu@gmail.com>
2018-09-04 15:47:25 -0400

    fixes #276 create http-url module for url related utility

--- utility/src/test/java/com/networknt/utility/StringUtilsTest.java
+++ utility/src/test/java/com/networknt/utility/StringUtilsTest.java
@@ -8,18 +8,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
-public class StringUtilTest {
+public class StringUtilsTest {
     //@Test
     public void testExpandEnvVars() {
         String s = "IP=${DOCKER_HOST_IP}";
-        Assert.assertEquals("IP=192.168.1.120", StringUtil.expandEnvVars(s));
+        Assert.assertEquals("IP=192.168.1.120", StringUtils.expandEnvVars(s));
     }
 
     @Test
     public void testInputStreamToString_withExpected() throws IOException {
         String expected = "test data";
         InputStream anyInputStream = new ByteArrayInputStream(expected.getBytes(StandardCharsets.UTF_8));
-        String actual = StringUtil.inputStreamToString(anyInputStream, StandardCharsets.UTF_8);
+        String actual = StringUtils.inputStreamToString(anyInputStream, StandardCharsets.UTF_8);
         Assert.assertEquals(expected, actual);
     }
 }
