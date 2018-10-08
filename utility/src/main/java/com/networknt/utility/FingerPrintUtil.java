d6d8724e15aff7885160883b785ae5846711314e
Nicholas Azar <NicholasAzar@users.noreply.github.com>
2018-10-08 09:31:42 -0400

    Update FingerPrintUtil to not depend on sun library toHexString, and add test cases with sample files

--- utility/src/main/java/com/networknt/utility/FingerPrintUtil.java
+++ utility/src/main/java/com/networknt/utility/FingerPrintUtil.java
@@ -6,10 +6,9 @@ import org.slf4j.LoggerFactory;
 import java.security.MessageDigest;
 import java.security.cert.Certificate;
 
-import static sun.security.pkcs11.wrapper.Functions.toHexString;
-
 public class FingerPrintUtil {
     static final Logger logger = LoggerFactory.getLogger(CodeVerifierUtil.class);
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
 
     public static String getCertFingerPrint(Certificate cert)  {
         byte [] digest = null;
@@ -20,6 +19,19 @@ public class FingerPrintUtil {
         } catch (Exception e) {
             logger.error("Exception:", e);
         }
-        return toHexString(digest);
+        if (digest != null) {
+            return bytesToHex(digest).toLowerCase();
+        }
+        return null;
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 }
