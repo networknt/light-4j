e1d1b577c5dafc5ab92751e04a3c39c1f20b7908
Nicholas Azar <NicholasAzar@users.noreply.github.com>
2018-10-08 11:49:30 -0400

    Fix failing test cases for status module

--- status/src/test/java/com/networknt/status/SeparateClassloaderTestRunner.java
+++ status/src/test/java/com/networknt/status/SeparateClassloaderTestRunner.java
@@ -3,7 +3,11 @@ package com.networknt.status;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.Paths;
 
 import static java.lang.ClassLoader.getSystemClassLoader;
 
@@ -24,7 +28,22 @@ public class SeparateClassloaderTestRunner  extends BlockJUnit4ClassRunner {
 
     public static class TestClassLoader extends URLClassLoader {
         public TestClassLoader() {
-            super(((URLClassLoader)getSystemClassLoader()).getURLs());
+            super(getSystemURLs(), getSystemClassLoader());
+//            super(((URLClassLoader)getSystemClassLoader()).getURLs());
+        }
+
+        private static URL[] getSystemURLs() {
+            String classpath = System.getProperty("java.class.path");
+            String[] entries = classpath.split(File.pathSeparator);
+            URL[] result = new URL[entries.length];
+            for(int i = 0; i < entries.length; i++) {
+                try {
+                    result[i] = Paths.get(entries[i]).toAbsolutePath().toUri().toURL();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+            return result;
         }
 
         @Override
