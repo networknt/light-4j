80151d69383b93deb2e17212f272b65bbd0eae25
Steve Hu <stevehu@gmail.com>
2018-11-02 12:32:20 -0400

    fixes #317 handle the situation that the alias is not server in the server.keystore

--- info/src/main/java/com/networknt/info/ServerInfoGetHandler.java
+++ info/src/main/java/com/networknt/info/ServerInfoGetHandler.java
@@ -165,12 +165,15 @@ public class ServerInfoGetHandler implements LightHttpHandler {
                 KeyStore loadedKeystore = KeyStore.getInstance("JKS");
                 loadedKeystore.load(stream, serverKeystorePass.toCharArray());
                 X509Certificate cert = (X509Certificate)loadedKeystore.getCertificate("server");
-                fingerPrint = FingerPrintUtil.getCertFingerPrint(cert);
+                if(cert != null) {
+                    fingerPrint = FingerPrintUtil.getCertFingerPrint(cert);
+                } else {
+                    logger.error("Unable to find the certificate with alias name as server in the keystore");
+                }
             } catch (Exception e) {
                 logger.error("Unable to load server keystore ", e);
             }
         }
         return fingerPrint;
     }
-
 }
