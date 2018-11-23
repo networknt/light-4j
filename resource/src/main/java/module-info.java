3d349fef21fc809a2d92ca564a4c2e4b8b21fd7f
Nicholas Azar <NicholasAzar@users.noreply.github.com>
2018-11-22 22:07:48 -0500

    Fixing test cases in resource module

--- resource/src/main/java/module-info.java
+++ resource/src/main/java/module-info.java
@@ -1,5 +1,8 @@
 module com.networknt.resource {
     exports com.networknt.resource;
 
+    requires com.networknt.config;
     requires undertow.core;
+    requires slf4j.api;
+    requires java.logging;
 }
