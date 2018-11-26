9233e6078e48905c9dcd48de14171516396e6d83
balloon <whenballoon@gmail.com>
2018-11-26 13:13:04 -0500

    -list headers under "headers" option will be filter out -tabSize is configurable -now can dump Status Code and Query Parameters correctly

--- dump/src/main/java/com/networknt/dump/DumpHelper.java
+++ dump/src/main/java/com/networknt/dump/DumpHelper.java
@@ -6,10 +6,9 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Map;
 
-public class DumpHelper {
+class DumpHelper {
 
-    static Logger logger = LoggerFactory.getLogger(DumpHelper.class);
-    private static int TAB_SIZE = 4;
+    private static Logger logger = LoggerFactory.getLogger(DumpHelper.class);
 
     static void dumpBasedOnOption(Object configObject, IDumpable dumpable) {
         if(configObject instanceof Boolean) {
@@ -27,24 +26,32 @@ public class DumpHelper {
         return IDumpable.HttpMessageType.RESPONSE.equals(type) ? DumpHandler.RESPONSE_OPTIONS : DumpHandler.REQUEST_OPTIONS;
     }
 
-    static void logResult(Map<String, Object> result, int level) {
+    static void logResult(Map<String, Object> result, int indentSize) {
+        int startLevel = -1;
+        _logResult(result, startLevel, indentSize);
+    }
+    private static void _logResult(Map<String, Object> result, int level, int indentSize) {
         level += 1;
         for(Map.Entry<String, Object> entry: result.entrySet()) {
             if(entry.getValue() instanceof Map<?, ?>) {
-                logger.info(getTabBasedOnLevel(level) + "{}:",entry.getKey());
-                logResult((Map)entry.getValue(), level);
+                logger.info(getTabBasedOnLevel(level, indentSize) + "{}:",entry.getKey());
+                _logResult((Map)entry.getValue(), level, indentSize);
             } else if(entry.getValue() instanceof String) {
-                logger.info(getTabBasedOnLevel(level) + "{}: {}",entry.getKey(), (String)entry.getValue());
-            } else {
-                logger.debug("Cannot handle this type: {}", entry.getKey());
+                logger.info(getTabBasedOnLevel(level, indentSize) + "{}: {}",entry.getKey(), entry.getValue(), indentSize);
+            } else if(entry.getValue() != null){
+                try{
+                    logger.info(getTabBasedOnLevel(level, indentSize) + "{}: {}", entry.getKey() , entry.getValue().toString());
+                } catch (Exception e) {
+                    logger.error("Cannot handle this type: {}", entry.getKey());
+                }
             }
         }
     }
 
-    static String getTabBasedOnLevel(int level) {
+    private static String getTabBasedOnLevel(int level, int indentSize) {
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < level; i ++) {
-            for(int j = 0; j < TAB_SIZE; j++) {
+            for(int j = 0; j < indentSize; j++) {
                 sb.append(" ");
             }
         }
@@ -52,7 +59,7 @@ public class DumpHelper {
     }
 
     //return true when an option is not written as 'true'
-    static Boolean checkOptionNotFalse(Object option) {
+    public static Boolean checkOptionNotFalse(Object option) {
         return (option instanceof Boolean && (Boolean) option)
                 || (!(option instanceof Boolean) && option != null);
     }
