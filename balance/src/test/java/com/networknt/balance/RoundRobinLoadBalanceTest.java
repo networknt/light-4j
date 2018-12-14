bf62dc07cc0dcad1463ec1d1bb96dc71f9ed1058
Steve Hu <stevehu@gmail.com>
2018-12-14 07:11:18 -0500

    fixes #335 make round robin load balance start with random number

--- balance/src/test/java/com/networknt/balance/RoundRobinLoadBalanceTest.java
+++ balance/src/test/java/com/networknt/balance/RoundRobinLoadBalanceTest.java
@@ -20,11 +20,14 @@ public class RoundRobinLoadBalanceTest {
     public void testSelect() throws Exception {
         List<URL> urls = new ArrayList<>();
 
-
         urls.add(new URLImpl("http", "127.0.0.1", 8081, "v1", new HashMap<String, String>()));
         urls.add(new URLImpl("http", "127.0.0.1", 8082, "v1", new HashMap<String, String>()));
         urls.add(new URLImpl("http", "127.0.0.1", 8083, "v1", new HashMap<String, String>()));
         urls.add(new URLImpl("http", "127.0.0.1", 8084, "v1", new HashMap<String, String>()));
+        while(true) {
+            URL url = loadBalance.select(urls, null);
+            if(url.getPort() == 8081) break;
+        }
         URL url = loadBalance.select(urls, null);
         Assert.assertEquals(url, URLImpl.valueOf("http://127.0.0.1:8082/v1"));
         url = loadBalance.select(urls, null);
@@ -42,5 +45,14 @@ public class RoundRobinLoadBalanceTest {
         List<URL> urls = new ArrayList<>();
         URL url = loadBalance.select(urls, null);
         Assert.assertNull(url);
+    }
+
+    @Test
+    public void testRandom() {
+        int r1 = (int)(Math.random()*50);
+        int r2 = (int)(Math.random()*50);
+        System.out.println("r1 = " + r1);
+        System.out.println("r2 = " + r2);
+        Assert.assertNotEquals(r1, r2);
     }
 }
