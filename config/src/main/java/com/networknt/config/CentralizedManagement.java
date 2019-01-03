af44bdf84a0a1c91a8d038a3ea8d999bd1090664
jiachen1120 <jiachen1120@gmail.com>
2019-01-03 14:57:52 -0500

    - to realize the discussion of the meeting on Jan.2 - test cases added - delete unused import - TODO: optimization

--- config/src/main/java/com/networknt/config/CentralizedManagement.java
+++ config/src/main/java/com/networknt/config/CentralizedManagement.java
@@ -1,9 +1,5 @@
 package com.networknt.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.*;
 
 public class CentralizedManagement {
