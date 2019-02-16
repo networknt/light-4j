280e40a3793614aa2f4b6cd68ec9f90373399202
Lan Phan <quoclan@gmail.com>
2019-02-16 09:49:09 +0700

    fix typo, simplify RoundRobinLoadBalanceTest, add license info (#396)

--- balance/src/test/java/com/networknt/balance/RoundRobinLoadBalanceTest.java
+++ balance/src/test/java/com/networknt/balance/RoundRobinLoadBalanceTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016 Network New Technologies Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.networknt.balance;
 
 import com.networknt.registry.URL;
@@ -15,7 +31,7 @@ import java.util.List;
  * Created by steve on 2016-12-07.
  */
 public class RoundRobinLoadBalanceTest {
-    LoadBalance loadBalance = (LoadBalance) SingletonServiceFactory.getBean(LoadBalance.class);
+    LoadBalance loadBalance = new RoundRobinLoadBalance();
 
     @Test
     public void testSelect() throws Exception {
