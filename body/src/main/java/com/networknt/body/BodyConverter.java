85297b2e5cb343ac89360eac6d8cf6e83400e817
Jeferson Viana Perito <jefperito@gmail.com>
2019-02-24 14:25:55 -0300

    Improving readbility - updating iteration of FormData item

--- body/src/main/java/com/networknt/body/BodyConverter.java
+++ body/src/main/java/com/networknt/body/BodyConverter.java
@@ -7,9 +7,7 @@ import java.util.*;
 public class BodyConverter {
     static Map<String, Object> convert(FormData data) {
         Map<String, Object> map = new HashMap<>();
-        Iterator<String> iterator = data.iterator();
-        while (iterator.hasNext()) {
-            String key = iterator.next();
+        for (String key : data) {
             List<Object> list = new ArrayList<>();
             for (FormData.FormValue value : data.get(key)) {
                 list.add(value);
