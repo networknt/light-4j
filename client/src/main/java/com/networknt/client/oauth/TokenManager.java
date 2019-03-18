c49e0e59ba0d1353ad8696d546a6152172d4d78e
Balloon <whenballoon@gmail.com>
2019-03-18 13:59:41 -0400

    -move cache config from oauth level under token level

--- client/src/main/java/com/networknt/client/oauth/TokenManager.java
+++ client/src/main/java/com/networknt/client/oauth/TokenManager.java
@@ -19,6 +19,7 @@ public class TokenManager {
     Map<String, Object> clientConfig = Config.getInstance().getJsonMapConfig(Http2Client.CONFIG_NAME);
     public static final String CACHE = "cache";
     public static final String OAUTH = "oauth";
+    public static final String TOKEN = "token";
     public static final String CAPACITY_CONFIG = "capacity";
 
     private static volatile TokenManager INSTANCE;
@@ -31,10 +32,13 @@ public class TokenManager {
         if(clientConfig != null) {
             Map<String, Object> oauthConfig = (Map<String, Object>)clientConfig.get(OAUTH);
             if(oauthConfig != null) {
-                Map<String, Object> cacheConfig = (Map<String, Object>)oauthConfig.get(CACHE);
-                if(cacheConfig != null) {
-                    if(cacheConfig.get(CAPACITY_CONFIG) != null) {
-                        CAPACITY = (Integer)cacheConfig.get(CAPACITY_CONFIG);
+                Map<String, Object> tokenConfig = (Map<String, Object>)oauthConfig.get(TOKEN);
+                if(tokenConfig != null) {
+                    Map<String, Object> cacheConfig = (Map<String, Object>)tokenConfig.get(CACHE);
+                    if(cacheConfig != null) {
+                        if(cacheConfig.get(CAPACITY_CONFIG) != null) {
+                            CAPACITY = (Integer)cacheConfig.get(CAPACITY_CONFIG);
+                        }
                     }
                 }
             }
