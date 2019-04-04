9c62da0394fe538c2d5cf81fb81c0cefe250fc2b
Jiachen Sun <31008528+jiachen1120@users.noreply.github.com>
2019-04-04 09:30:02 -0400

    Cache unparsed request body (configurable/ for application/json request) (#447)
    * Refactored the bodyHandler
    Cached unparsed Json body
    
    Todo: implemented caching unparsed FormData body
    
    * Added body caching test
    
    * Deleted necessary codes
    
    * renamed test
    
    * optimized the code and docs
    
    * Fixed test conflicts
    
    * update the license header
    
    * remove enabled check and change the attachment to String

---
+++ body/src/test/java/com/networknt/body/BodyStringCachingTest.java
@@ -0,0 +1,145 @@
+package com.networknt.body;
+
+import com.networknt.client.Http2Client;
+import com.networknt.exception.ClientException;
+import io.undertow.Handlers;
+import io.undertow.Undertow;
+import io.undertow.client.ClientConnection;
+import io.undertow.client.ClientRequest;
+import io.undertow.client.ClientResponse;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.RoutingHandler;
+import io.undertow.util.Headers;
+import io.undertow.util.Methods;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xnio.IoUtils;
+import org.xnio.OptionMap;
+
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class BodyStringCachingTest {
+    static final Logger logger = LoggerFactory.getLogger(BodyHandlerTest.class);
+
+    static Undertow server = null;
+
+    @BeforeClass
+    public static void setUp() {
+        if (server == null) {
+            logger.info("starting server");
+            HttpHandler handler = getTestHandler();
+            BodyHandler bodyHandler = new BodyHandler();
+            bodyHandler.setNext(handler);
+            handler = bodyHandler;
+            server = Undertow.builder()
+                    .addHttpListener(8080, "localhost")
+                    .setHandler(handler)
+                    .build();
+            server.start();
+        }
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        if (server != null) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ignored) {
+
+            }
+            server.stop();
+            logger.info("The server is stopped.");
+        }
+    }
+
+    static RoutingHandler getTestHandler() {
+        return Handlers.routing()
+                .add(Methods.POST, "/post", exchange -> {
+                    String bodyString = (String) exchange.getAttachment(BodyHandler.REQUEST_BODY_STRING);
+                    if (bodyString == null) {
+                        exchange.getResponseSender().send("nobody");
+                    } else {
+                        exchange.getResponseSender().send(bodyString);
+                    }
+                });
+    }
+
+    @Test
+    public void testEnableCacheRequestBody() throws Exception {
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
+        String post;
+        try {
+            connection = client.connect(new URI("http://localhost:8080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+        BodyHandler.config.setCacheRequestBody(true);
+        try {
+            post = "[{\"key1\":\"value1\"}, {\"key2\":\"value2\"}]";
+            connection.getIoThread().execute(new Runnable() {
+                @Override
+                public void run() {
+                    final ClientRequest request = new ClientRequest().setMethod(Methods.POST).setPath("/post");
+                    request.getRequestHeaders().put(Headers.HOST, "localhost");
+                    request.getRequestHeaders().put(Headers.CONTENT_TYPE, "application/json");
+                    request.getRequestHeaders().put(Headers.TRANSFER_ENCODING, "chunked");
+                    connection.sendRequest(request, client.createClientCallback(reference, latch, post));
+                }
+            });
+
+            latch.await(10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            logger.error("IOException: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        Assert.assertEquals(post, reference.get().getAttachment(Http2Client.RESPONSE_BODY));
+    }
+
+    @Test
+    public void testDisableCacheRequestBody() throws Exception {
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
+        String post;
+        try {
+            connection = client.connect(new URI("http://localhost:8080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+        BodyHandler.config.setCacheRequestBody(false);
+        try {
+            post = "[{\"key1\":\"value1\"}, {\"key2\":\"value2\"}]";
+            connection.getIoThread().execute(new Runnable() {
+                @Override
+                public void run() {
+                    final ClientRequest request = new ClientRequest().setMethod(Methods.POST).setPath("/post");
+                    request.getRequestHeaders().put(Headers.HOST, "localhost");
+                    request.getRequestHeaders().put(Headers.CONTENT_TYPE, "application/json");
+                    request.getRequestHeaders().put(Headers.TRANSFER_ENCODING, "chunked");
+                    connection.sendRequest(request, client.createClientCallback(reference, latch, post));
+                }
+            });
+
+            latch.await(10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            logger.error("IOException: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        Assert.assertEquals("nobody", reference.get().getAttachment(Http2Client.RESPONSE_BODY));
+    }
+}
