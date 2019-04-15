150ecf5fd2f8b0a304d07ac9c6302283b3b8f85e
jiachen1120 <jiachen1120@gmail.com>
2019-04-15 16:34:03 -0400

    Added comments TODO: 1. added tests for tokenManager 2. added tests for multi-thread

--- client/src/main/java/com/networknt/client/oauth/TokenManager.java
+++ client/src/main/java/com/networknt/client/oauth/TokenManager.java
@@ -32,15 +32,15 @@ public class TokenManager {
 
     private TokenManager() {
         //set CAPACITY based on config
-        if(clientConfig != null) {
-            Map<String, Object> oauthConfig = (Map<String, Object>)clientConfig.get(OAUTH);
-            if(oauthConfig != null) {
-                Map<String, Object> tokenConfig = (Map<String, Object>)oauthConfig.get(TOKEN);
-                if(tokenConfig != null) {
-                    Map<String, Object> cacheConfig = (Map<String, Object>)tokenConfig.get(CACHE);
-                    if(cacheConfig != null) {
-                        if(cacheConfig.get(CAPACITY_CONFIG) != null) {
-                            CAPACITY = (Integer)cacheConfig.get(CAPACITY_CONFIG);
+        if (clientConfig != null) {
+            Map<String, Object> oauthConfig = (Map<String, Object>) clientConfig.get(OAUTH);
+            if (oauthConfig != null) {
+                Map<String, Object> tokenConfig = (Map<String, Object>) oauthConfig.get(TOKEN);
+                if (tokenConfig != null) {
+                    Map<String, Object> cacheConfig = (Map<String, Object>) tokenConfig.get(CACHE);
+                    if (cacheConfig != null) {
+                        if (cacheConfig.get(CAPACITY_CONFIG) != null) {
+                            CAPACITY = (Integer) cacheConfig.get(CAPACITY_CONFIG);
                         }
                     }
                 }
@@ -50,9 +50,9 @@ public class TokenManager {
     }
 
     public static TokenManager getInstance() {
-        if(INSTANCE == null) {
+        if (INSTANCE == null) {
             synchronized (TokenManager.class) {
-                if(INSTANCE == null) {
+                if (INSTANCE == null) {
                     INSTANCE = new TokenManager();
                 }
             }
@@ -63,12 +63,13 @@ public class TokenManager {
     /**
      * get a Jwt with a provided Key (Key is either scope or a service id inputted by user, for caching usage):
      * 1.if a token is cached with provided key
-     *      - if the token is expired, renew it right away.
-     *      - if the token is almost expired, use this token and renew it silently.
-     *      - if the token is not almost expired, just use this token.
+     * - if the token is expired, renew it right away.
+     * - if the token is almost expired, use this token and renew it silently.
+     * - if the token is not almost expired, just use this token.
      * 2.if a token is not cached with provided key
-     *      - get a new jwt from oauth server
+     * - get a new jwt from oauth server
      * 3.after getting the valid token, cache that token no matter if it's already cached or not. The strategy should determine how to cache it.
+     *
      * @param key either based on scope or service id
      * @return a Jwt if successful, otherwise return error Status.
      */
@@ -83,6 +84,15 @@ public class TokenManager {
         return result;
     }
 
+    /**
+     * get a Jwt with a provided cache key and a token request:
+     * 1. if the provided key can be cached, populate Jwt with cache strategy
+     * 2. if the provided key cannot be cached, populate Jwt without caching
+     *
+     * @param key          a key based on service id, scope, authorization code, refresh token, saml assertion or client claim
+     * @param tokenRequest a token request that contains the components needed by oauth server
+     * @return a Jwt if successful, otherwise return error Status.
+     */
     public Result<Jwt> getJwt(Jwt.Key key, TokenRequest tokenRequest) {
         if (key.isCachable()) {
             Jwt cachedJwt = getJwt(cacheStrategy, key);
@@ -97,7 +107,7 @@ public class TokenManager {
             // uncached jwt
             Jwt jwt = new Jwt();
             Result<TokenResponse> result = OauthHelper.getTokenResult(tokenRequest);
-            if(result.isSuccess()) {
+            if (result.isSuccess()) {
                 TokenResponse tokenResponse = result.getResult();
                 jwt.setJwt(tokenResponse.getAccessToken());
                 jwt.setRefreshToken(tokenResponse.getRefreshToken());
@@ -112,7 +122,7 @@ public class TokenManager {
     //cache jwt if not exist
     private synchronized Jwt getJwt(ICacheStrategy cacheStrategy, Jwt.Key key) {
         Jwt result = cacheStrategy.getCachedJwt(key);
-        if(result == null) {
+        if (result == null) {
             //cache an empty JWT first.
             result = new Jwt(key);
             cacheStrategy.cacheJwt(key, result);
@@ -124,16 +134,17 @@ public class TokenManager {
      * get a Jwt with a provided clientRequest,
      * it will get token based on Jwt.Key (either scope or service_id)
      * if the user declared both scope and service_id in header, it will get jwt based on scope
+     *
      * @param clientRequest
      * @return
      */
     public Result<Jwt> getJwt(ClientRequest clientRequest) {
         HeaderValues scope = clientRequest.getRequestHeaders().get(OauthHelper.SCOPE);
-        if(scope != null) {
+        if (scope != null) {
             return getJwt(new Jwt.Key(scope.getFirst()));
         }
         HeaderValues serviceId = clientRequest.getRequestHeaders().get(OauthHelper.SERVICE_ID);
-        if(serviceId != null) {
+        if (serviceId != null) {
             return getJwt(new Jwt.Key(serviceId.getFirst()));
         }
         return getJwt(new Jwt.Key());
