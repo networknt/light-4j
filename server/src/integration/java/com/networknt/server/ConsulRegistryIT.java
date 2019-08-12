c0819092b75e7ddd0004f18be1f800c87c431fa2
jiachen1120 <jiachen1120@gmail.com>
2019-08-12 14:52:48 -0400

    - Fix consul integration test by skip service unregistering

--- server/src/integration/java/com/networknt/server/ConsulRegistryIT.java
+++ server/src/integration/java/com/networknt/server/ConsulRegistryIT.java
@@ -88,10 +88,14 @@ public class ConsulRegistryIT {
         startServer(49588, null, server1);
         startServer(49589, "", server2);
         startServer(49590, "dev", server3);
+        // Wait for server start complete
+        Thread.sleep(10000);
     }
 
     @AfterClass
     public static void tearDown() {
+        // Skip the service unregistering since the test container tear down before the server tear down
+        serverConfig.setEnableRegistry(false);
         if (server1 != null) {
             try {
                 Thread.sleep(1000);
@@ -125,12 +129,9 @@ public class ConsulRegistryIT {
     public void testDiscoverWithoutEnvTag() throws InterruptedException {
         String url = null;
         for (int i = 0; i < retryTimes; i++) {
-            try {
-                url = lightCluster.serviceToUrl("https", serviceId, null, null);
-                if (url != null) break;
-            } catch (Exception e) {
-                Thread.sleep(2000);
-            }
+            url = lightCluster.serviceToUrl("https", serviceId, null, null);
+            if (url != null) break;
+            Thread.sleep(2000);
         }
         Assert.assertNotNull(url);
         Assert.assertTrue(url.equals("https://" + ipAddress + ":49588") || url.equals("https://" + ipAddress + ":49589") || url.equals("https://" + ipAddress + ":49590"));
@@ -140,12 +141,9 @@ public class ConsulRegistryIT {
     public void testDiscoverWithEmptyStringEnvTag() throws InterruptedException {
         String url = null;
         for (int i = 0; i < retryTimes; i++) {
-            try {
-                url = lightCluster.serviceToUrl("https", serviceId, "", null);
-                if (url != null) break;
-            } catch (Exception e) {
-                Thread.sleep(2000);
-            }
+            url = lightCluster.serviceToUrl("https", serviceId, "", null);
+            if (url != null) break;
+            Thread.sleep(2000);
         }
         Assert.assertNotNull(url);
         Assert.assertTrue(url.equals("https://" + ipAddress + ":49588") || url.equals("https://" + ipAddress + ":49589"));
@@ -155,12 +153,9 @@ public class ConsulRegistryIT {
     public void testDiscoverWithNonEmptyStringEnvTag() throws InterruptedException {
         String url = null;
         for (int i = 0; i < retryTimes; i++) {
-            try {
-                url = lightCluster.serviceToUrl("https", serviceId, "dev", null);
-                if (url != null) break;
-            } catch (Exception e) {
-                Thread.sleep(2000);
-            }
+            url = lightCluster.serviceToUrl("https", serviceId, "dev", null);
+            if (url != null) break;
+            Thread.sleep(2000);
         }
         Assert.assertNotNull(url);
         Assert.assertEquals("https://" + ipAddress + ":49590", url);
@@ -176,12 +171,9 @@ public class ConsulRegistryIT {
     public void testDiscoverCaching() throws InterruptedException {
         String url = null;
         for (int i = 0; i < retryTimes; i++) {
-            try {
-                url = lightCluster.serviceToUrl("https", serviceId, "dev", null);
-                if (url != null) break;
-            } catch (Exception e) {
-                Thread.sleep(2000);
-            }
+            url = lightCluster.serviceToUrl("https", serviceId, "dev", null);
+            if (url != null) break;
+            Thread.sleep(2000);
         }
         Assert.assertNotNull(url);
         Assert.assertEquals("https://" + ipAddress + ":49590", url);
@@ -201,6 +193,6 @@ public class ConsulRegistryIT {
         logger.info("starting server");
         new Thread(() -> server.start()).start();
         // Wait for server start complete
-        Thread.sleep(5000);
+        Thread.sleep(1000);
     }
 }
