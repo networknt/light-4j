fe2e77f483536cf5c136dbbd696bb7b1350324f7
Dan Dobrin <20207377+ddobrin@users.noreply.github.com>
2019-08-28 19:03:51 -0400

    fixes #607 Collect Status and StackTrace on Error in light-4j handlers for auditing purposes

--- http-string/src/main/java/com/networknt/httpstring/AttachmentConstants.java
+++ http-string/src/main/java/com/networknt/httpstring/AttachmentConstants.java
@@ -4,9 +4,18 @@ import io.opentracing.Span;
 import io.opentracing.Tracer;
 import io.undertow.util.AttachmentKey;
 
+import java.util.Map;
+
+/**
+ * This class allows the definition of attachment keys shared by multiple modules and thus eliminates dependencies between them
+ *
+ * @author ddobrin
+ *
+ */
 public class AttachmentConstants {
     // The key to the root span for this particular request attachment in exchange.
     public static final AttachmentKey<Span> ROOT_SPAN = AttachmentKey.create(Span.class);
     public static final AttachmentKey<Tracer> EXCHANGE_TRACER = AttachmentKey.create(Tracer.class);
-
+    // The key to the audit info attachment in exchange. Allow other handlers to set values.
+    public static final AttachmentKey<Map> AUDIT_INFO = AttachmentKey.create(Map.class);
 }
