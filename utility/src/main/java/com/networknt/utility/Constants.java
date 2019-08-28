27d602851f1ea240a396ae427496f59bdfead0fa
Dan Dobrin <20207377+ddobrin@users.noreply.github.com>
2019-08-28 19:03:51 -0400

    fixes #607 Collect Status and StackTrace on Error in light-4j handlers for auditing purposes

--- utility/src/main/java/com/networknt/utility/Constants.java
+++ utility/src/main/java/com/networknt/utility/Constants.java
@@ -43,8 +43,10 @@ public class Constants {
 
     public static final String CSRF_TOKEN_STRING = "X-CSRF-TOKEN";
 
-    // Logger
+    // Logging and Auditing
     public static final String AUDIT_LOGGER = "Audit";
+    public static final String STATUS = "Status";
+    public static final String STACK_TRACE = "StackTrace";
 
     // JWT claims for fine-grained authorization in business context.
     // The key for the subject token claims in auditInfo after security handler
