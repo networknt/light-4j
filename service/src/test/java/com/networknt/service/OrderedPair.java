61ae40a996b66ddf31853c00461d76fd53082554
Steve Hu <stevehu@gmail.com>
2019-12-30 14:08:03 -0500

    fixes #635 add a test case with two generic types for service module (#636)

---
+++ service/src/test/java/com/networknt/service/OrderedPair.java
@@ -0,0 +1,14 @@
+package com.networknt.service;
+
+public class OrderedPair<K, V> implements Pair<K, V> {
+    private K key;
+    private V value;
+
+    public OrderedPair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey()	{ return key; }
+    public V getValue() { return value; }
+}
---
+++ service/src/test/java/com/networknt/service/Pair.java
@@ -0,0 +1,7 @@
+package com.networknt.service;
+
+public interface Pair<K, V> {
+    K getKey();
+    V getValue();
+}
+
