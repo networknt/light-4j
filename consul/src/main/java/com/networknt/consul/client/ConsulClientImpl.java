96a50966bf983db2858f45be76bd9ec898f9d104
Gavin Chen <gavinyan.han@gmail.com>
2020-02-22 18:09:05 -0500

    fixes for #649

--- consul/src/main/java/com/networknt/consul/client/ConsulClientImpl.java
+++ consul/src/main/java/com/networknt/consul/client/ConsulClientImpl.java
@@ -237,7 +237,7 @@ public class ConsulClientImpl implements ConsulClient {
 	private ConsulConnection getConnection(String cacheKey) {
 		//the case when enable http2 support use the class level ConsulConnection
         // will use http/2 connection only if tls is enabled as Consul only support HTTP/2 with TLS.
-		if(config.isEnableHttp2() && config.getConsulUrl().toLowerCase().startsWith("https")) {
+		if(config.isEnableHttp2() || config.getConsulUrl().toLowerCase().startsWith("https")) {
 			return this.http2Connection;
 		} else {
 			ConsulConnection cachedConsulConnection = connectionPool.get(cacheKey);
