18704715b21592879e9e2dbf0de5d1ea1e717ebe
Gavin Chen <gavinyan.han@gmail.com>
2020-07-24 17:31:45 -0400

    content type support

--- body/src/main/java/com/networknt/body/ContentType.java
+++ body/src/main/java/com/networknt/body/ContentType.java
@@ -11,25 +11,46 @@ public class ContentType {
 
     private final String type;
 
-    private final String subtype;
+
+    public static final String ALL_VALUE = "*/*";
+
+    public static final String APPLICATION_ATOM_XML_VALUE = "application/atom+xml";
+
+    public static final String APPLICATION_CBOR_VALUE = "application/cbor";
+
+    public static final String APPLICATION_FORM_URLENCODED_VALUE = "application/x-www-form-urlencoded";
+
+    public static final String APPLICATION_JSON_VALUE = "application/json";
+
+    public static final String APPLICATION_PDF_VALUE = "application/pdf";
+
+    public static final String APPLICATION_STREAM_JSON_VALUE = "application/stream+json";
+
+    public static final String APPLICATION_XML_VALUE = "application/xml";
+
+    public static final String MULTIPART_FORM_DATA_VALUE = "multipart/form-data";
+
+    public static final String MULTIPART_MIXED_VALUE = "multipart/mixed";
+
+    public static final String TEXT_HTML_VALUE = "text/html";
+
+    public static final String TEXT_PLAIN_VALUE = "text/plain";
+
+    public static final String TEXT_XML_VALUE = "text/xml";
+
+    public static final String IMAGE_PNG_VALUE = "image/png";
+
+    public static final String IMAGE_JPEG_VALUE = "image/jpeg";
+
+    public static final String IMAGE_GIF_VALUE = "image/gif";
 
     public ContentType(String type) {
-        this(type, WILDCARD_TYPE);
-    }
-
-    public ContentType(String type, String subtype) {
         this.type = type;
-        this.subtype = subtype;
     }
-
-
-
 
     public String getType() {
         return type;
     }
 
-    public String getSubtype() {
-        return subtype;
-    }
+
 }
