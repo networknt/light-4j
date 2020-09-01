b3abae17359cb4715b984ff8944fb9d9df57fffc
Steve Hu <stevehu@gmail.com>
2020-09-01 16:10:06 -0400

    fixes #763 add client authenticated user request in the OauthHelper for light-spa-4j (#764)

--- client/src/main/java/com/networknt/client/oauth/ClientRequestComposerProvider.java
+++ client/src/main/java/com/networknt/client/oauth/ClientRequestComposerProvider.java
@@ -11,8 +11,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.networknt.client.oauth.ClientRequestComposerProvider.ClientRequestComposers.CLIENT_CREDENTIAL_REQUEST_COMPOSER;
-import static com.networknt.client.oauth.ClientRequestComposerProvider.ClientRequestComposers.SAML_BEARER_REQUEST_COMPOSER;
+import static com.networknt.client.oauth.ClientRequestComposerProvider.ClientRequestComposers.*;
 
 /**
  * This class is a singleton to provide registered IClientRequestComposable composers.
@@ -22,7 +21,7 @@ import static com.networknt.client.oauth.ClientRequestComposerProvider.ClientReq
  * To see composer please check {@link com.networknt.client.oauth.IClientRequestComposable}
  */
 public class ClientRequestComposerProvider {
-    public enum ClientRequestComposers { CLIENT_CREDENTIAL_REQUEST_COMPOSER, SAML_BEARER_REQUEST_COMPOSER }
+    public enum ClientRequestComposers { CLIENT_CREDENTIAL_REQUEST_COMPOSER, SAML_BEARER_REQUEST_COMPOSER, CLIENT_AUTHENTICATED_USER_REQUEST_COMPOSER }
     private static final ClientRequestComposerProvider INSTANCE = new ClientRequestComposerProvider();
     private Map<ClientRequestComposers, IClientRequestComposable> composersMap = new HashMap<>();
     private static final Logger logger = LoggerFactory.getLogger(ClientRequestComposerProvider.class);
@@ -53,6 +52,9 @@ public class ClientRequestComposerProvider {
                 break;
             case SAML_BEARER_REQUEST_COMPOSER:
                 composersMap.put(SAML_BEARER_REQUEST_COMPOSER, new DefaultSAMLBearerRequestComposer());
+                break;
+            case CLIENT_AUTHENTICATED_USER_REQUEST_COMPOSER:
+                composersMap.put(CLIENT_AUTHENTICATED_USER_REQUEST_COMPOSER, new DefaultClientAuthenticatedUserRequestComposer());
                 break;
             default:
                 break;
@@ -125,4 +127,31 @@ public class ClientRequestComposerProvider {
             return "";
         }
     }
+
+    /**
+     * the default composer to compose a ClientRequest with the given TokenRequest to get ClientAuthenticatedUser token.
+     */
+    private static class DefaultClientAuthenticatedUserRequestComposer implements IClientRequestComposable {
+
+        @Override
+        public ClientRequest composeClientRequest(TokenRequest tokenRequest) {
+            final ClientRequest request = new ClientRequest().setMethod(Methods.POST).setPath(tokenRequest.getUri());
+            request.getRequestHeaders().put(Headers.HOST, "localhost");
+            request.getRequestHeaders().put(Headers.TRANSFER_ENCODING, "chunked");
+            request.getRequestHeaders().put(Headers.CONTENT_TYPE, "application/x-www-form-urlencoded");
+            request.getRequestHeaders().put(Headers.AUTHORIZATION, OauthHelper.getBasicAuthHeader(tokenRequest.getClientId(), tokenRequest.getClientSecret()));
+            return request;
+        }
+
+        @Override
+        public String composeRequestBody(TokenRequest tokenRequest) {
+            try {
+                return OauthHelper.getEncodedString(tokenRequest);
+            } catch (UnsupportedEncodingException e) {
+                logger.error("get encoded string from tokenRequest fails: \n {}", e.toString());
+            }
+            return "";
+        }
+    }
+
 }
