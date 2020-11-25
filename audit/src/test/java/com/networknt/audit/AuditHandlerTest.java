7bc7e478448b88c53ca26386aca997f87f4aece5
Jiachen Sun <jiachen1120@gmail.com>
2020-11-25 15:10:07 -0500

    - fixed #839 Error status resulted at framework middleware handler is missing in auditInfo - Added unit test

--- audit/src/test/java/com/networknt/audit/AuditHandlerTest.java
+++ audit/src/test/java/com/networknt/audit/AuditHandlerTest.java
@@ -26,7 +26,9 @@ import com.networknt.config.JsonMapper;
 import com.networknt.correlation.CorrelationHandler;
 import com.networknt.exception.ClientException;
 import com.networknt.handler.Handler;
+import com.networknt.handler.LightHttpHandler;
 import com.networknt.httpstring.HttpStringConstants;
+import com.networknt.status.Status;
 import com.networknt.utility.Constants;
 import io.undertow.Handlers;
 import io.undertow.Undertow;
@@ -40,6 +42,7 @@ import io.undertow.util.AttachmentKey;
 import io.undertow.util.HeaderMap;
 import io.undertow.util.Headers;
 import io.undertow.util.Methods;
+import javafx.scene.effect.Light;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.mockito.*;
@@ -54,12 +57,14 @@ import org.xnio.OptionMap;
 
 import java.net.URI;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
+import static com.networknt.audit.AuditConfig.CONFIG_NAME;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -86,7 +91,8 @@ public class AuditHandlerTest {
                                 Constants.CORRELATION_ID_STRING,
                                 Constants.TRACEABILITY_ID_STRING,
                                 AuditHandler.STATUS_CODE,
-                                AuditHandler.RESPONSE_TIME };
+                                AuditHandler.RESPONSE_TIME
+                             };
 
     @BeforeClass
     public static void setUp() {
@@ -125,7 +131,8 @@ public class AuditHandlerTest {
 
     static RoutingHandler getTestHandler() {
         return Handlers.routing()
-                .add(Methods.POST, "/pet", exchange -> exchange.getResponseSender().send("OK"));
+                .add(Methods.POST, "/pet", exchange -> exchange.getResponseSender().send("OK"))
+                .add(Methods.POST, "/error", exchange -> new ErrorStatusTestHandler().handleRequest(exchange));
     }
 
     @Before
@@ -150,6 +157,14 @@ public class AuditHandlerTest {
         Assert.assertEquals(traceVal, mapValue.get(Constants.TRACEABILITY_ID_STRING));
         Assert.assertNotNull(mapValue.get(Constants.CORRELATION_ID_STRING));
         Assert.assertEquals(200, mapValue.get(AuditHandler.STATUS_CODE));
+    }
+
+    private void verifyAuditErrorStatus() {
+        verify(mockAppender, times(1)).doAppend(captorLoggingEvent.capture());
+        ILoggingEvent event = captorLoggingEvent.getValue();
+        Map<String, Object> mapValue = JsonMapper.string2Map(event.getFormattedMessage());
+
+        Assert.assertEquals("{statusCode=401, code=ERR10001, severity=ERROR, message=AUTH_TOKEN_EXPIRED, description=Jwt token in authorization header expired}", mapValue.get("Status").toString());
     }
 
     @Test
@@ -235,6 +250,48 @@ public class AuditHandlerTest {
         } catch (InterruptedException ignored) {
         }
         verifyAuditLog(null);
+    }
+
+    @Test
+    public void testAuditWithErrorStatus() throws Exception {
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
+        try {
+            connection = client.connect(new URI("http://localhost:8080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+
+        try {
+            String post = "post";
+            connection.getIoThread().execute(new Runnable() {
+                @Override
+                public void run() {
+                    final ClientRequest request = new ClientRequest().setMethod(Methods.POST).setPath("/error");
+                    request.getRequestHeaders().put(Headers.HOST, "localhost");
+                    request.getRequestHeaders().put(Headers.AUTHORIZATION, "Bearer eyJraWQiOiIxMDAiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ1cm46Y29tOm5ldHdvcmtudDpvYXV0aDI6djEiLCJhdWQiOiJ1cm46Y29tLm5ldHdvcmtudCIsImV4cCI6MTc5MDAzNTcwOSwianRpIjoiSTJnSmdBSHN6NzJEV2JWdUFMdUU2QSIsImlhdCI6MTQ3NDY3NTcwOSwibmJmIjoxNDc0Njc1NTg5LCJ2ZXJzaW9uIjoiMS4wIiwidXNlcl9pZCI6InN0ZXZlIiwidXNlcl90eXBlIjoiRU1QTE9ZRUUiLCJjbGllbnRfaWQiOiJmN2Q0MjM0OC1jNjQ3LTRlZmItYTUyZC00YzU3ODc0MjFlNzIiLCJzY29wZSI6WyJ3cml0ZTpwZXRzIiwicmVhZDpwZXRzIl19.mue6eh70kGS3Nt2BCYz7ViqwO7lh_4JSFwcHYdJMY6VfgKTHhsIGKq2uEDt3zwT56JFAePwAxENMGUTGvgceVneQzyfQsJeVGbqw55E9IfM_uSM-YcHwTfR7eSLExN4pbqzVDI353sSOvXxA98ZtJlUZKgXNE1Ngun3XFORCRIB_eH8B0FY_nT_D1Dq2WJrR-re-fbR6_va95vwoUdCofLRa4IpDfXXx19ZlAtfiVO44nw6CS8O87eGfAm7rCMZIzkWlCOFWjNHnCeRsh7CVdEH34LF-B48beiG5lM7h4N12-EME8_VDefgMjZ8eqs1ICvJMxdIut58oYbdnkwTjkA");
+                    request.getRequestHeaders().put(HttpStringConstants.SCOPE_TOKEN, "Bearer eyJraWQiOiIxMDAiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ1cm46Y29tOm5ldHdvcmtudDpvYXV0aDI6djEiLCJhdWQiOiJ1cm46Y29tLm5ldHdvcmtudCIsImV4cCI6MTc5MDAzNTcwOSwianRpIjoiSTJnSmdBSHN6NzJEV2JWdUFMdUU2QSIsImlhdCI6MTQ3NDY3NTcwOSwibmJmIjoxNDc0Njc1NTg5LCJ2ZXJzaW9uIjoiMS4wIiwidXNlcl9pZCI6InN0ZXZlIiwidXNlcl90eXBlIjoiRU1QTE9ZRUUiLCJjbGllbnRfaWQiOiJmN2Q0MjM0OC1jNjQ3LTRlZmItYTUyZC00YzU3ODc0MjFlNzIiLCJzY29wZSI6WyJ3cml0ZTpwZXRzIiwicmVhZDpwZXRzIl19.mue6eh70kGS3Nt2BCYz7ViqwO7lh_4JSFwcHYdJMY6VfgKTHhsIGKq2uEDt3zwT56JFAePwAxENMGUTGvgceVneQzyfQsJeVGbqw55E9IfM_uSM-YcHwTfR7eSLExN4pbqzVDI353sSOvXxA98ZtJlUZKgXNE1Ngun3XFORCRIB_eH8B0FY_nT_D1Dq2WJrR-re-fbR6_va95vwoUdCofLRa4IpDfXXx19ZlAtfiVO44nw6CS8O87eGfAm7rCMZIzkWlCOFWjNHnCeRsh7CVdEH34LF-B48beiG5lM7h4N12-EME8_VDefgMjZ8eqs1ICvJMxdIut58oYbdnkwTjkA");
+                    request.getRequestHeaders().put(Headers.TRANSFER_ENCODING, "chunked");
+                    connection.sendRequest(request, client.createClientCallback(reference, latch, post));
+                }
+            });
+
+            latch.await(10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            logger.error("IOException: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        Assert.assertEquals(401, reference.get().getResponseCode());
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException ignored) {
+        }
+        verifyAuditErrorStatus();
     }
 
     @Test
@@ -368,4 +425,6 @@ public class AuditHandlerTest {
             return attachmentKey.toString().equals("io.undertow.util.SimpleAttachmentKey<java.lang.Integer>");
         }
     }
+
+
 }
