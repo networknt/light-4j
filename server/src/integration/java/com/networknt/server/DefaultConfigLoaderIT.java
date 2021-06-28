3b3214c29ae172c2e0a4a1d013b43f91d1e9433c
Steve Hu <stevehu@gmail.com>
2021-06-28 14:26:27 -0400

    fixes #973 switch to jdk 11 http client to connect to the config server (#974)

--- server/src/integration/java/com/networknt/server/DefaultConfigLoaderIT.java
+++ server/src/integration/java/com/networknt/server/DefaultConfigLoaderIT.java
@@ -6,6 +6,6 @@ import org.junit.Test;
 public class DefaultConfigLoaderIT {
     @Test
     public void testConfigServerHealth() throws Exception {
-        Assert.assertEquals("OK", DefaultConfigLoader.getConfigServerHealth("https://localhost:8443", "/config-server/health"));
+        Assert.assertEquals("OK", DefaultConfigLoader.getConfigServerHealth("https://localhost:8443", "/health/liveness/com.networknt.config-server-1.0.0"));
     }
 }
