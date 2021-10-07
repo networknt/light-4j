9cc4c4d96ebadd020314976890bf11d9194aedc8
Steve Hu <stevehu@gmail.com>
2021-10-07 16:01:37 -0400

    fixes #1042 add another method and test cases

--- utility/src/main/java/com/networknt/utility/TimeUtil.java
+++ utility/src/main/java/com/networknt/utility/TimeUtil.java
@@ -34,4 +34,36 @@ public class TimeUtil {
         }
         return millisecond;
     }
+
+    /**
+     * Get the rounded-up timestamp in millisecond by the TimeUnit based on a start time.
+     * @param timeUnit TimeUnit
+     * @param start start time in millisecond
+     * @return the next rounded up millisecond for the TimeUnit.
+     */
+    public static long nextStartTimestamp(TimeUnit timeUnit, long start) {
+        long nextTimestamp = start;
+        switch (timeUnit) {
+            case MILLISECONDS:
+                break;
+
+            case SECONDS:
+                nextTimestamp = 1000 + 1000 * (start / 1000); // the next second is the start timestamp.
+                break;
+
+            case MINUTES:
+                nextTimestamp = 60000 + 60000 * (start / 60000); // next minute is the start timestamp
+                break;
+
+            case HOURS:
+                nextTimestamp = 3600000 + 3600000 * (start / 3600000); // next hour is the start timestamp
+                break;
+
+            case DAYS:
+                nextTimestamp = 86400000 + 86400000 * (start / 86400000); // next day is the start timestamp
+                break;
+        }
+        return nextTimestamp;
+    }
+
 }
