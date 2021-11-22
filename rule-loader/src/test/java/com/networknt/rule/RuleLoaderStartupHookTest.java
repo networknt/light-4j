abfa3ac4c2cd6bb724e50698d85cee04fca309db
Steve Hu <stevehu@gmail.com>
2021-11-22 14:19:33 -0500

    disable the rule-loader startup hook as it depends on portal

--- rule-loader/src/test/java/com/networknt/rule/RuleLoaderStartupHookTest.java
+++ rule-loader/src/test/java/com/networknt/rule/RuleLoaderStartupHookTest.java
@@ -1,9 +1,11 @@
 package com.networknt.rule;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class RuleLoaderStartupHookTest {
     @Test
+    @Ignore
     public void testRuleLoader() {
         RuleLoaderStartupHook ruleLoaderStartupHook = new RuleLoaderStartupHook();
         ruleLoaderStartupHook.onStartup();
