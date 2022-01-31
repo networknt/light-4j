64ee3ed9bac9d769a975366900acea7b8d1892df
Steve Hu <stevehu@gmail.com>
2022-01-31 18:51:56 -0500

    implemented the dynamic salt with ramdom generator

--- common/src/test/java/com/networknt/common/AESSaltEncryptor.java
+++ common/src/test/java/com/networknt/common/AESSaltEncryptor.java
@@ -1,81 +1,56 @@
-/*
- * Copyright (c) 2016 Network New Technologies Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.networknt.common;
+
+import com.networknt.utility.Constants;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.AlgorithmParameters;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidParameterSpecException;
+import java.security.spec.KeySpec;
 
 import static com.networknt.decrypt.Decryptor.CRYPT_PREFIX;
 import static java.lang.System.exit;
 
-import java.io.UnsupportedEncodingException;
-import java.security.AlgorithmParameters;
-import java.security.InvalidKeyException;
-import java.security.spec.InvalidParameterSpecException;
-import java.security.spec.KeySpec;
-import java.util.Base64;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.SecretKeySpec;
-
-import com.networknt.utility.Constants;
-
 /**
- * Please use AESSaltEncryptor instead.
- *
+ * This is a new encryptor utility to encrypt the secret with stronger implementation.
+ * Instead of using a static salt, we are generating a salt for each invocation to make
+ * dictionary attack harder.
  */
-@Deprecated
-public class AESEncryptor {
+public class AESSaltEncryptor {
     public static void main(String [] args) {
         if(args.length == 0) {
             System.out.println("Please provide plain text to encrypt!");
             exit(0);
         }
-        AESEncryptor encryptor = new AESEncryptor();
+        AESSaltEncryptor encryptor = new AESSaltEncryptor();
         System.out.println(encryptor.encrypt(args[0]));
     }
 
     private static final int ITERATIONS = 65536;
     private static final int KEY_SIZE = 128;
-    private static final byte[] SALT = { (byte) 0x0, (byte) 0x0, (byte) 0x0, (byte) 0x0, (byte) 0x0, (byte) 0x0, (byte) 0x0, (byte) 0x0 };
     private static final String STRING_ENCODING = "UTF-8";
+    private static byte[] salt;
     private SecretKeySpec secret;
     private Cipher cipher;
-    private Base64.Encoder base64Encoder;
 
-    public AESEncryptor() {
+    public AESSaltEncryptor() {
         try {
-           /* Derive the key, given password and salt. */
+            /* Derive the key, given password and salt. */
             SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-            KeySpec spec;
-
-            spec = new PBEKeySpec(Constants.FRAMEWORK_NAME.toCharArray(), SALT, ITERATIONS, KEY_SIZE);
+            salt = getSalt();
+            KeySpec spec = new PBEKeySpec(Constants.FRAMEWORK_NAME.toCharArray(), salt, ITERATIONS, KEY_SIZE);
             SecretKey tmp = factory.generateSecret(spec);
             secret = new SecretKeySpec(tmp.getEncoded(), "AES");
-
             // CBC = Cipher Block chaining
             // PKCS5Padding Indicates that the keys are padded
             cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-
-            // For production use commons base64 encoder
-            base64Encoder = Base64.getEncoder();
         } catch (Exception e) {
             throw new RuntimeException("Unable to initialize", e);
         }
@@ -102,7 +77,7 @@ public class AESEncryptor {
             byte[] out = new byte[iv.length + ciphertext.length];
             System.arraycopy(iv, 0, out, 0, iv.length);
             System.arraycopy(ciphertext, 0, out, iv.length, ciphertext.length);
-            return CRYPT_PREFIX + ":" + base64Encoder.encodeToString(out);
+            return CRYPT_PREFIX + ":" + toHex(salt) + ":" +toHex(out);
         } catch (IllegalBlockSizeException e) {
             throw new RuntimeException("Unable to encrypt", e);
         } catch (BadPaddingException e) {
@@ -113,6 +88,30 @@ public class AESEncryptor {
             throw new RuntimeException("Unable to encrypt", e);
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException("Unable to encrypt", e);
+        } catch(NoSuchAlgorithmException e) {
+            throw new RuntimeException("Unable to encrypt", e);
+        }
+    }
+
+    private static byte[] getSalt() throws NoSuchAlgorithmException
+    {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt;
+    }
+
+    private static String toHex(byte[] array) throws NoSuchAlgorithmException
+    {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+
+        int paddingLength = (array.length * 2) - hex.length();
+        if(paddingLength > 0)
+        {
+            return String.format("%0"  +paddingLength + "d", 0) + hex;
+        }else{
+            return hex;
         }
     }
 }
---
+++ decryptor/src/main/java/com/networknt/decrypt/AESSaltDecryptor.java
@@ -0,0 +1,99 @@
+package com.networknt.decrypt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.KeySpec;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * This implementation is replaced by AESSaltDecryptor with dynamic salt instead of static one.
+ * It allows different application to use different salt so that it is harder to any attacker
+ * to perform dictionary attack.
+ */
+public class AESSaltDecryptor implements Decryptor {
+    private static final Logger logger = LoggerFactory.getLogger(AESSaltDecryptor.class);
+
+    private static final int ITERATIONS = 65536;
+    private static final String STRING_ENCODING = "UTF-8";
+    // cache the secret to void recreating instances for each decrypt call as all config files
+    // will use the same salt per application.
+    private Map<String, SecretKeySpec> secretMap = new ConcurrentHashMap<>();
+    /**
+     * If we user Key size of 256 we will get java.security.InvalidKeyException:
+     * Illegal key size or default parameters , Unless we configure Java
+     * Cryptography Extension 128
+     */
+    private static final int KEY_SIZE = 128;
+    private Cipher cipher;
+
+
+    public AESSaltDecryptor() {
+        try {
+            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        } catch (Exception e) {
+            logger.error("Failed to get the Cipher instance:", e);
+            throw new RuntimeException("Unable to initialize", e);
+        }
+    }
+
+    @Override
+    public String decrypt(String input) {
+        if (!input.startsWith(CRYPT_PREFIX)) {
+            logger.error("The secret text is not started with prefix " + CRYPT_PREFIX);
+            throw new RuntimeException("Unable to decrypt, input string does not start with 'CRYPT'.");
+        }
+        String[] parts = input.split(":");
+        // need to make sure that the salt is in the secret text.
+        if(parts.length != 3) {
+            logger.error("The secret text is not formatted correctly with CRYPT:salt:hash");
+            throw new RuntimeException("Unable to decrypt, input string is not formatted correctly with CRYPT:salt:hash");
+        }
+
+        try {
+            byte[] salt = fromHex(parts[1]);
+            byte[] hash = fromHex(parts[2]);
+            // try to get the secret from the cache first.
+            SecretKeySpec secret = secretMap.get(parts[1]);
+            if(secret == null) {
+                KeySpec spec = new PBEKeySpec(getPassword(), salt, ITERATIONS, KEY_SIZE);
+                /* Derive the key, given password and salt. */
+                SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+                SecretKey tmp = factory.generateSecret(spec);
+                secret = new SecretKeySpec(tmp.getEncoded(), "AES");
+                secretMap.put(parts[1], secret);
+            }
+            // CBC = Cipher Block chaining
+            // PKCS5Padding Indicates that the keys are padded
+            int keylen = KEY_SIZE / 8;
+            byte[] iv = new byte[keylen];
+            System.arraycopy(hash, 0, iv, 0, keylen);
+            cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv));
+            return new String(cipher.doFinal(hash, keylen, hash.length - keylen), STRING_ENCODING);
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to decrypt because the decrypted password is incorrect.", e);
+        }
+    }
+
+    protected char[] getPassword() {
+        return "light".toCharArray();
+    }
+
+    private static byte[] fromHex(String hex) throws NoSuchAlgorithmException
+    {
+        byte[] bytes = new byte[hex.length() / 2];
+        for(int i = 0; i < bytes.length ;i++)
+        {
+            bytes[i] = (byte)Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+        }
+        return bytes;
+    }
+}
