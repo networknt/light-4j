fc92f26e951514d30d2a3b45644e9ef3865c7240
Steve Hu <stevehu@gmail.com>
2022-01-31 18:51:56 -0500

    implemented the dynamic salt with ramdom generator

--- common/src/test/java/com/networknt/common/DecryptUtilTest.java
+++ common/src/test/java/com/networknt/common/DecryptUtilTest.java
@@ -17,16 +17,91 @@
 package com.networknt.common;
 
 import com.networknt.config.Config;
+import com.networknt.decrypt.AESSaltDecryptor;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.spec.KeySpec;
+import java.util.Base64;
 import java.util.Map;
 
 public class DecryptUtilTest {
+    private static final String SECRET_KEY = "my_super_secret_key";
+    private static final String SALT = "ssshhhhhhhhhhh!!!!";
+
     @Test
     public void testDecryptMap() {
         Map<String, Object> secretMap = Config.getInstance().getJsonMapConfig("secret");
         DecryptUtil.decryptMap(secretMap);
         Assert.assertEquals("password", secretMap.get("serverKeystorePass"));
+    }
+
+    @Test
+    public void testAESSaltEncryptor() {
+        String p = "password";
+        AESSaltEncryptor encryptor = new AESSaltEncryptor();
+        String s = encryptor.encrypt(p);
+        System.out.println("s = " + s);
+        AESSaltDecryptor decryptor = new AESSaltDecryptor();
+        String c = decryptor.decrypt(s);
+        System.out.println("c = " + c);
+    }
+
+    @Test
+    public void test256() {
+        String originalString = "howtodoinjava.com";
+
+        String encryptedString = encrypt(originalString);
+        String decryptedString = decrypt(encryptedString);
+
+        System.out.println(originalString);
+        System.out.println(encryptedString);
+        System.out.println(decryptedString);
+    }
+
+    public static String encrypt(String strToEncrypt) {
+        try {
+            byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALT.getBytes(), 65536, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+            return Base64.getEncoder()
+                    .encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
+        } catch (Exception e) {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    public static String decrypt(String strToDecrypt) {
+        try {
+            byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALT.getBytes(), 65536, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        } catch (Exception e) {
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
     }
 }
