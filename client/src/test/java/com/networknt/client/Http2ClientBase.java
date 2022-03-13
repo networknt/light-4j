b9060d2261a83c7c85e76da8916ad0cce6586bf9
Steve Hu <stevehu@gmail.com>
2022-03-13 18:34:29 -0400

    Issue1139 (#1145)
    * fixes #1139 update the client.yml to support multiple auth servers
    
    * fixes #1138 update the client module to get tokens based on path

---
+++ client/src/test/java/com/networknt/client/Http2ClientBase.java
@@ -0,0 +1,211 @@
+package com.networknt.client;
+
+import com.networknt.config.Config;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.MalformedClaimException;
+import org.jose4j.jwt.NumericDate;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.jose4j.jwt.consumer.JwtContext;
+import org.jose4j.lang.JoseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xnio.IoUtils;
+import org.xnio.OptionMap;
+
+import javax.net.ssl.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.interfaces.RSAPrivateKey;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Http2ClientBase {
+    private static Logger logger = LoggerFactory.getLogger(Http2ClientBase.class);
+
+    private static final char[] STORE_PASSWORD = "password".toCharArray();
+
+    protected static KeyStore loadKeyStore(final String name) throws IOException {
+        final InputStream stream = Config.getInstance().getInputStreamFromFile(name);
+        if(stream == null) {
+            throw new RuntimeException("Could not load keystore");
+        }
+        try {
+            KeyStore loadedKeystore = KeyStore.getInstance("JKS");
+            loadedKeystore.load(stream, STORE_PASSWORD);
+
+            return loadedKeystore;
+        } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException e) {
+            throw new IOException(String.format("Unable to load KeyStore %s", name), e);
+        } finally {
+            IoUtils.safeClose(stream);
+        }
+    }
+
+    protected static SSLContext createSSLContext(final KeyStore keyStore, final KeyStore trustStore, boolean client) throws IOException {
+        KeyManager[] keyManagers;
+        try {
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory.init(keyStore, STORE_PASSWORD);
+            keyManagers = keyManagerFactory.getKeyManagers();
+        } catch (NoSuchAlgorithmException | UnrecoverableKeyException | KeyStoreException e) {
+            throw new IOException("Unable to initialise KeyManager[]", e);
+        }
+
+        TrustManager[] trustManagers = null;
+        try {
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(trustStore);
+            trustManagers = trustManagerFactory.getTrustManagers();
+        } catch (NoSuchAlgorithmException | KeyStoreException e) {
+            throw new IOException("Unable to initialise TrustManager[]", e);
+        }
+
+        SSLContext sslContext;
+        try {
+            if(!client) {
+                sslContext = SSLContext.getInstance("TLS");
+            } else {
+                sslContext = SSLContext.getInstance("TLS");
+            }
+            sslContext.init(keyManagers, trustManagers, null);
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new IOException("Unable to create and initialise the SSLContext", e);
+        }
+
+        return sslContext;
+    }
+
+    protected static boolean isTokenExpired(String authorization) {
+        boolean expired = false;
+        String jwt = getJwtFromAuthorization(authorization);
+        if(jwt != null) {
+            try {
+                JwtConsumer consumer = new JwtConsumerBuilder()
+                        .setSkipAllValidators()
+                        .setDisableRequireSignature()
+                        .setSkipSignatureVerification()
+                        .build();
+
+                JwtContext jwtContext = consumer.process(jwt);
+                JwtClaims jwtClaims = jwtContext.getJwtClaims();
+
+                try {
+                    if ((NumericDate.now().getValue() - 60) >= jwtClaims.getExpirationTime().getValue()) {
+                        expired = true;
+                    }
+                } catch (MalformedClaimException e) {
+                    logger.error("MalformedClaimException:", e);
+                }
+            } catch(InvalidJwtException e) {
+                e.printStackTrace();
+            }
+        }
+        return expired;
+    }
+
+    protected static String getJwtFromAuthorization(String authorization) {
+        String jwt = null;
+        if(authorization != null) {
+            String[] parts = authorization.split(" ");
+            if (parts.length == 2) {
+                String scheme = parts[0];
+                String credentials = parts[1];
+                Pattern pattern = Pattern.compile("^Bearer$", Pattern.CASE_INSENSITIVE);
+                if (pattern.matcher(scheme).matches()) {
+                    jwt = credentials;
+                }
+            }
+        }
+        return jwt;
+    }
+
+    public static String getJwt(JwtClaims claims) throws JoseException {
+        String jwt;
+
+        RSAPrivateKey privateKey = (RSAPrivateKey) getPrivateKey(
+                "/config/primary.jks", "password", "selfsigned");
+
+        // A JWT is a JWS and/or a JWE with JSON claims as the payload.
+        // In this example it is a JWS nested inside a JWE
+        // So we first create a JsonWebSignature object.
+        JsonWebSignature jws = new JsonWebSignature();
+
+        // The payload of the JWS is JSON content of the JWT Claims
+        jws.setPayload(claims.toJson());
+
+        // The JWT is signed using the sender's private key
+        jws.setKey(privateKey);
+        jws.setKeyIdHeaderValue("100");
+
+        // Set the signature algorithm on the JWT/JWS that will integrity protect the claims
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256);
+
+        // Sign the JWS and produce the compact serialization, which will be the inner JWT/JWS
+        // representation, which is a string consisting of three dot ('.') separated
+        // base64url-encoded parts in the form Header.Payload.Signature
+        jwt = jws.getCompactSerialization();
+        return jwt;
+    }
+
+    private static PrivateKey getPrivateKey(String filename, String password, String key) {
+        PrivateKey privateKey = null;
+
+        try {
+            KeyStore keystore = KeyStore.getInstance("JKS");
+            keystore.load(Http2Client.class.getResourceAsStream(filename),
+                    password.toCharArray());
+
+            privateKey = (PrivateKey) keystore.getKey(key,
+                    password.toCharArray());
+        } catch (Exception e) {
+            logger.error("Exception:", e);
+        }
+
+        if (privateKey == null) {
+            logger.error("Failed to retrieve private key from keystore");
+        }
+
+        return privateKey;
+    }
+
+    protected static String getJwt(int expiredInSeconds) throws Exception {
+        JwtClaims claims = getTestClaims();
+        claims.setExpirationTime(NumericDate.fromMilliseconds(System.currentTimeMillis() + expiredInSeconds * 1000));
+        return getJwt(claims);
+    }
+
+    protected static JwtClaims getTestClaims() {
+        JwtClaims claims = new JwtClaims();
+        claims.setIssuer("urn:com:networknt:oauth2:v1");
+        claims.setAudience("urn:com.networknt");
+        claims.setExpirationTimeMinutesInTheFuture(10);
+        claims.setGeneratedJwtId(); // a unique identifier for the token
+        claims.setIssuedAtToNow();  // when the token was issued/created (now)
+        claims.setNotBeforeMinutesInThePast(2); // time before which the token is not yet valid (2 minutes ago)
+        claims.setClaim("version", "1.0");
+
+        claims.setClaim("user_id", "steve");
+        claims.setClaim("user_type", "EMPLOYEE");
+        claims.setClaim("client_id", "aaaaaaaa-1234-1234-1234-bbbbbbbb");
+        List<String> scope = Arrays.asList("api.r", "api.w");
+        claims.setStringListClaim("scope", scope); // multi-valued claims work too and will end up as a JSON array
+        return claims;
+    }
+
+    static Http2Client createClient() {
+        return createClient(OptionMap.EMPTY);
+    }
+
+    static Http2Client createClient(final OptionMap options) {
+        return Http2Client.getInstance();
+    }
+
+
+}
