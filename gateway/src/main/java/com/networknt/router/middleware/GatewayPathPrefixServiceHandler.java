74de71143be25c5718b2e8cccee823983d1496fe
Steve Hu <stevehu@gmail.com>
2022-06-14 20:57:02 -0400

    fixes #1279 update path service handlers with new config class to support map from config server (#1280)

--- gateway/src/main/java/com/networknt/router/middleware/GatewayPathPrefixServiceHandler.java
+++ gateway/src/main/java/com/networknt/router/middleware/GatewayPathPrefixServiceHandler.java
@@ -16,37 +16,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * This is the same handler as the ServiceDictHandler used in the client proxy and gateway;
+ * This is the same handler as the PathPrefixServiceHandler used in the client proxy and gateway;
  * however, it has logic to detect the traffic is incoming or outgoing. This handler is used for
  * outgoing traffic only for service to service invocation.
  * It depends on the gateway.yml to detect the traffic based on either request header or protocol.
  *
- * @author Gavin Chen
+ * @author Steve Hu
  */
-public class GatewayServiceDictHandler extends ServiceDictHandler {
-    private static final Logger logger = LoggerFactory.getLogger(GatewayServiceDictHandler.class);
+public class GatewayPathPrefixServiceHandler extends PathPrefixServiceHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GatewayPathPrefixServiceHandler.class);
 
     private GatewayConfig gatewayConfig;
 
-    public GatewayServiceDictHandler() {
-        logger.info("GatewayServiceDictHandler is constructed");
-        config = ServiceDictConfig.load();
-        gatewayConfig = (GatewayConfig)Config.getInstance().getJsonObjectConfig(GatewayConfig.CONFIG_NAME, GatewayConfig.class);
+    public GatewayPathPrefixServiceHandler() {
+        logger.info("GatewayPathPrefixServiceHandler is constructed");
+        config = PathPrefixServiceConfig.load();
+        gatewayConfig = (GatewayConfig) Config.getInstance().getJsonObjectConfig(GatewayConfig.CONFIG_NAME, GatewayConfig.class);
     }
 
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
         if (Constants.HEADER.equalsIgnoreCase(gatewayConfig.getEgressIngressIndicator())) {
-            serviceDict(exchange);
+            pathPrefixService(exchange);
         } else if (Constants.PROTOCOL.equalsIgnoreCase(gatewayConfig.getEgressIngressIndicator()) && HttpURL.PROTOCOL_HTTP.equalsIgnoreCase(exchange.getRequestScheme())){
-            serviceDict(exchange);
+            pathPrefixService(exchange);
         } else {
             // incoming request, let the proxy handler to handle it.
             Handler.next(exchange, next);
         }
     }
 
-    protected void serviceDict(HttpServerExchange exchange) throws Exception {
+    protected void pathPrefixService(HttpServerExchange exchange) throws Exception {
         String[] serviceEntry = null;
         HeaderValues serviceUrlHeader = exchange.getRequestHeaders().get(HttpStringConstants.SERVICE_URL);
         String serviceUrl = serviceUrlHeader != null ? serviceUrlHeader.peekFirst() : null;
@@ -55,8 +55,7 @@ public class GatewayServiceDictHandler extends ServiceDictHandler {
             String serviceId = serviceIdHeader != null ? serviceIdHeader.peekFirst() : null;
             if (serviceId == null) {
                 String requestPath = exchange.getRequestURI();
-                String httpMethod = exchange.getRequestMethod().toString().toLowerCase();
-                serviceEntry = HandlerUtils.findServiceEntry(toInternalKey(httpMethod, requestPath), config.getMapping());
+                serviceEntry = HandlerUtils.findServiceEntry(HandlerUtils.normalisePath(requestPath), config.getMapping());
                 if (serviceEntry != null) {
                     exchange.getRequestHeaders().put(HttpStringConstants.SERVICE_ID, serviceEntry[1]);
                 }
@@ -66,7 +65,7 @@ public class GatewayServiceDictHandler extends ServiceDictHandler {
         if(auditInfo == null) {
             // AUDIT_INFO is created for light-gateway to populate the endpoint as the OpenAPI handlers might not be available.
             auditInfo = new HashMap<>();
-            auditInfo.put(Constants.ENDPOINT_STRING, serviceEntry[0]);
+            auditInfo.put(Constants.ENDPOINT_STRING, serviceEntry[0] + "@" + exchange.getRequestMethod().toString().toLowerCase());
             exchange.putAttachment(AttachmentConstants.AUDIT_INFO, auditInfo);
         }
         Handler.next(exchange, this.next);
