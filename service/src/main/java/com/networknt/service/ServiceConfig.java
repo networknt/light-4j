bb71ff7ba83a299c898c0c0083e6371508e91c4c
Steve Hu <stevehu@gmail.com>
2022-06-17 16:48:50 -0400

    fixes #1292 upload ServiceConfig to support load singletons as JSON string (#1293)

--- service/src/main/java/com/networknt/service/ServiceConfig.java
+++ service/src/main/java/com/networknt/service/ServiceConfig.java
@@ -16,6 +16,12 @@
 
 package com.networknt.service;
 
+import com.networknt.config.Config;
+import com.networknt.config.ConfigException;
+import com.networknt.config.JsonMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 import java.util.Map;
 
@@ -25,16 +31,50 @@ import java.util.Map;
  * @author Steve Hu
  */
 public class ServiceConfig {
-    List<Map<String, Object>> singletons;
+    private static final Logger logger = LoggerFactory.getLogger(ServiceConfig.class);
 
-    public ServiceConfig() {
+    public static final String CONFIG_NAME = "service";
+    public static final String SINGLETONS = "singletons";
+    private List<Map<String, Object>> singletons;
+    private Config config;
+    private Map<String, Object> mappedConfig;
+
+    private ServiceConfig() {
+        this(CONFIG_NAME);
+    }
+
+    private ServiceConfig(String configName) {
+        config = Config.getInstance();
+        mappedConfig = config.getJsonMapConfigNoCache(configName);
+        setConfigData();
+    }
+
+    public static ServiceConfig load() {
+        return new ServiceConfig();
+    }
+
+    public static ServiceConfig load(String configName) {
+        return new ServiceConfig(configName);
+    }
+
+    public Map<String, Object> getMappedConfig() {
+        return mappedConfig;
     }
 
     public List<Map<String, Object>> getSingletons() {
         return singletons;
     }
 
-    public void setSingletons(List<Map<String, Object>> singletons) {
-        this.singletons = singletons;
+    public void setConfigData() {
+        if(mappedConfig.get(SINGLETONS) instanceof String) {
+            // the json string is supported here.
+            String s = (String)mappedConfig.get(SINGLETONS);
+            if(logger.isTraceEnabled()) logger.trace("singletons = " + s);
+            singletons = JsonMapper.string2List(s);
+        } else if (mappedConfig.get(SINGLETONS) instanceof List) {
+            singletons = (List<Map<String, Object>>)mappedConfig.get(SINGLETONS);
+        } else {
+            throw new ConfigException("singletons missing or wrong type.");
+        }
     }
 }
