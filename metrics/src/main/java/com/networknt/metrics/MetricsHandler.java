2b5aa38c53b56f7074ad97fa7ea1158ef48510a5
dudan <dudan@sinnbo.com>
2022-08-19 13:39:11 +0800

    fix the potential risks of ExchangeCompleteListener

--- metrics/src/main/java/com/networknt/metrics/MetricsHandler.java
+++ metrics/src/main/java/com/networknt/metrics/MetricsHandler.java
@@ -123,20 +123,25 @@ public class MetricsHandler implements MiddlewareHandler {
     public void handleRequest(final HttpServerExchange exchange) throws Exception {
         long startTime = Clock.defaultClock().getTick();
         exchange.addExchangeCompleteListener((exchange1, nextListener) -> {
-            Map<String, Object> auditInfo = exchange1.getAttachment(AttachmentConstants.AUDIT_INFO);
-            if(auditInfo != null) {
-                Map<String, String> tags = new HashMap<>();
-                tags.put("endpoint", (String)auditInfo.get(Constants.ENDPOINT_STRING));
-                tags.put("clientId", auditInfo.get(Constants.CLIENT_ID_STRING) != null ? (String)auditInfo.get(Constants.CLIENT_ID_STRING) : "unknown");
+            try {
+                Map<String, Object> auditInfo = exchange1.getAttachment(AttachmentConstants.AUDIT_INFO);
+                if(auditInfo != null) {
+                    Map<String, String> tags = new HashMap<>();
+                    tags.put("endpoint", (String)auditInfo.get(Constants.ENDPOINT_STRING));
+                    tags.put("clientId", auditInfo.get(Constants.CLIENT_ID_STRING) != null ? (String)auditInfo.get(Constants.CLIENT_ID_STRING) : "unknown");
 
-                long time = Clock.defaultClock().getTick() - startTime;
-                MetricName metricName = new MetricName("response_time");
-                metricName = metricName.tagged(commonTags);
-                metricName = metricName.tagged(tags);
-                registry.getOrAdd(metricName, MetricRegistry.MetricBuilder.TIMERS).update(time, TimeUnit.NANOSECONDS);
-                incCounterForStatusCode(exchange1.getStatusCode(), commonTags, tags);
+                    long time = Clock.defaultClock().getTick() - startTime;
+                    MetricName metricName = new MetricName("response_time");
+                    metricName = metricName.tagged(commonTags);
+                    metricName = metricName.tagged(tags);
+                    registry.getOrAdd(metricName, MetricRegistry.MetricBuilder.TIMERS).update(time, TimeUnit.NANOSECONDS);
+                    incCounterForStatusCode(exchange1.getStatusCode(), commonTags, tags);
+                }
+            }  catch (Throwable e) {
+                logger.error("ExchangeListenerThrowable", e);
+            } finally {
+                nextListener.proceed();
             }
-            nextListener.proceed();
         });
         
         Handler.next(exchange, next);
