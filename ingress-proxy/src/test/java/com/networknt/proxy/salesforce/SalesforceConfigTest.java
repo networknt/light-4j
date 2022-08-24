7d3dc76827cf89dae96082702413ab14686bbfb1
Steve Hu <stevehu@gmail.com>
2022-08-24 16:41:27 -0400

    fixes #1344 update salesforce handler to support multiple APIs with different authentication parameters

--- ingress-proxy/src/test/java/com/networknt/proxy/salesforce/SalesforceConfigTest.java
+++ ingress-proxy/src/test/java/com/networknt/proxy/salesforce/SalesforceConfigTest.java
@@ -1,22 +1,20 @@
 package com.networknt.proxy.salesforce;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.List;
 
 public class SalesforceConfigTest {
     @Test
-    @Ignore
     public void testConfigLoad() {
         SalesforceConfig config = SalesforceConfig.load();
-        Assert.assertEquals(3, config.getPathPrefixAuth().size());
-        Assert.assertTrue(config.getMorningStar().size()  == 3);
-        Assert.assertTrue(config.getConquest().size() == 3);
-        Assert.assertTrue(config.getAdvisorHub().size() == 3);
-
-        Assert.assertTrue(config.getMorningStar().get(config.IV) != null);
-        Assert.assertTrue(config.getConquest().get(config.IV) != null);
-        Assert.assertTrue(config.getAdvisorHub().get(config.IV) != null);
+        Assert.assertEquals(2, config.getPathPrefixAuths().size());
+        List<PathPrefixAuth> pathPrefixAuthList = config.getPathPrefixAuths();
+        Assert.assertTrue(pathPrefixAuthList.get(0).getAuthAudience() != null);
+        Assert.assertTrue(pathPrefixAuthList.get(0).getAuthIssuer() != null);
+        Assert.assertTrue(pathPrefixAuthList.get(0).getAuthSubject() != null);
+        Assert.assertTrue(pathPrefixAuthList.get(0).getIv() != null);
+        Assert.assertTrue(pathPrefixAuthList.get(0).getServiceHost() != null);
     }
-
 }
