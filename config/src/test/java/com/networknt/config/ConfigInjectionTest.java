165caa44c52822457e4a9aed674076ddaf1af4ba
AkashWorkGit <107428154+AkashWorkGit@users.noreply.github.com>
2022-09-12 20:31:50 -0400

    Changes made to the code as required. Mentioned in issue #1274 & #1287 (#1357)
    * Changes made to the code as per the requirements mentioned in Issue #1274 & Issue #1287
    
    * Changes made to the code as per the requirements mentioned in Issue #1274 & Issue #1287
    
    * Made a static object for Decryptor to be used in the class since while starting the service it was creating multiple instances of the decryptor.
    
    Co-authored-by: Akash <akash.sharma@cibc.com>

--- config/src/test/java/com/networknt/config/ConfigInjectionTest.java
+++ config/src/test/java/com/networknt/config/ConfigInjectionTest.java
@@ -1,5 +1,7 @@
 package com.networknt.config;
 
+import com.networknt.decrypt.Decryptor;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -36,4 +38,68 @@ public class ConfigInjectionTest {
         assertNotNull(newConfigValue);
         assertEquals(configValue, newConfigValue);
     }
+
+    @Test
+    public void testConvertEnvVarsUsingDotInValue() {
+        String testInput = ConfigInjection.convertEnvVars("server.environment");
+        Assert.assertEquals("SERVER_ENVIRONMENT", testInput);
+    }
+
+    @Test
+    public void testConvertEnvVarsUsingDotInValueWithMixCases() {
+        String testInput = ConfigInjection.convertEnvVars("serVER.ENVironment");
+        Assert.assertEquals("SERVER_ENVIRONMENT", testInput);
+    }
+
+    @Test
+    public void testConvertEnvVarsUsingDotInValueWithCamelCasing() {
+        String testInput = ConfigInjection.convertEnvVars("server.ENVIRONMENT");
+        Assert.assertEquals("SERVER_ENVIRONMENT", testInput);
+    }
+
+
+    @Test
+    public void testConvertEnvVarsUsingNullValue() {
+        String testInput2 = ConfigInjection.convertEnvVars(null);
+        Assert.assertEquals(null, testInput2);
+    }
+
+    @Test
+    public void testConvertEnvVarsUsingEmptyString() {
+        String testInput3 = ConfigInjection.convertEnvVars("");
+        Assert.assertEquals("", testInput3);
+    }
+
+    @Test
+    public void testDecryptEnvValueWithEncryptedValue() {
+
+        Decryptor aesDecryptor = ConfigInjection.getDecryptor();
+        Object envValue = ConfigInjection.decryptEnvValue(aesDecryptor, "CRYPT:EqDVC30YKUDTMLXSIS5OpOqeP+K4w0dPaFfaJPfzIT8=");
+        Assert.assertEquals("password", envValue);
+    }
+
+    @Test
+    public void testDecryptEnvValueWithNonEncryptedValue() {
+
+        Decryptor aesDecryptor = ConfigInjection.getDecryptor();
+        Object envValue = ConfigInjection.decryptEnvValue(aesDecryptor, "password");
+        Assert.assertEquals("password", envValue);
+    }
+
+    @Test
+    public void testDecryptEnvValueWithEmptyValue() {
+
+        Decryptor aesDecryptor = ConfigInjection.getDecryptor();
+        Object envValue = ConfigInjection.decryptEnvValue(aesDecryptor, "");
+        Assert.assertEquals("", envValue);
+    }
+
+    @Test
+    public void testDecryptEnvValueWithNullValue() {
+
+        Decryptor aesDecryptor = ConfigInjection.getDecryptor();
+        Object envValue = ConfigInjection.decryptEnvValue(aesDecryptor, null);
+        Assert.assertEquals(null, envValue);
+    }
+
 }
