457c2a8f04e49f857e2af8f248476d78e52d5c91
Steve Hu <stevehu@gmail.com>
2022-11-12 12:45:24 -0500

    fixes #1476 Update middleware handlers to register the module after reload (#1477)

--- egress-router/src/main/java/com/networknt/router/RouterHandler.java
+++ egress-router/src/main/java/com/networknt/router/RouterHandler.java
@@ -30,18 +30,18 @@ import org.xnio.OptionMap;
 /**
  * This is a wrapper class for ProxyHandler as it is implemented as final. This class implements
  * the HttpHandler which can be injected into the handler.yml configuration file as another option
- * for the handlers injection. The other option is to use RouterHandlerProvider in service.yml file.
+ * for the handler injection. The other option is to use RouterHandlerProvider in service.yml file.
  *
  * @author Steve Hu
  */
 public class RouterHandler implements HttpHandler {
-    private RouterConfig config;
+    private static RouterConfig config;
 
     protected ProxyHandler proxyHandler;
 
     public RouterHandler() {
         config = RouterConfig.load();
-        ModuleRegistry.registerModule(RouterHandler.class.getName(), Config.getInstance().getJsonMapConfigNoCache(RouterConfig.CONFIG_NAME), null);
+        ModuleRegistry.registerModule(RouterHandler.class.getName(), config.getMappedConfig(), null);
         // As we are building a client side router for the light platform, the assumption is the server will
         // be on HTTP 2.0 TSL always. No need to handle HTTP 1.1 case here.
         LoadBalancingRouterProxyClient client = new LoadBalancingRouterProxyClient();
@@ -72,6 +72,6 @@ public class RouterHandler implements HttpHandler {
 
     public void reload() {
         config.reload();
+        ModuleRegistry.registerModule(RouterHandler.class.getName(), config.getMappedConfig(), null);
     }
-
 }
