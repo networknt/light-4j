fed44b8dbaa3bf18a2de65b7cf1e707484e63b1f
Yihen Liu <tt0suzy@wirelessprv-10-192-162-3.near.illinois.edu>
2022-11-23 19:42:47 -0600

    fixed flaky test in URLTest.java

--- registry/src/test/java/com/networknt/registry/URLTest.java
+++ registry/src/test/java/com/networknt/registry/URLTest.java
@@ -99,10 +99,10 @@ public class URLTest {
         Assert.assertTrue(canServe);
 
         String fullStr = url.toFullStr();
-        Assert.assertEquals("http://localhost:7080/config?key1=value1&key2=true&key5=10L&key3=10&key4=3.14&nodeType=service&version=1.0&", fullStr);
+        Assert.assertEquals("http://localhost:7080/config?key1=value1&key2=true&key3=10&key4=3.14&key5=10L&nodeType=service&version=1.0&", fullStr);
 
 
-        URL newUrl = URLImpl.valueOf("http://localhost:7080/config?key1=value1&key2=true&key5=10L&key3=10&key4=3.14&nodeType=service&version=1.0");
+        URL newUrl = URLImpl.valueOf("http://localhost:7080/config?key1=value1&key2=true&key3=10&key4=3.14&key5=10L&nodeType=service&version=1.0&");
 
         Assert.assertNotNull(newUrl);
     }
