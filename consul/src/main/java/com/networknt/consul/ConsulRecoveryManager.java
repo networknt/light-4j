6fe7fadf8a88ca96d11873f8a98f666fe1a5b89e
Michael Christoff <mike.christoff@cibc.com>
2022-12-10 22:53:34 -0500

    Updated ConsulRecoveryManager to start the ConsulThreadMonitor thread and to support Consul lookup thread checkins

--- consul/src/main/java/com/networknt/consul/ConsulRecoveryManager.java
+++ consul/src/main/java/com/networknt/consul/ConsulRecoveryManager.java
@@ -3,21 +3,34 @@ package com.networknt.consul;
 import com.networknt.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class ConsulRecoveryManager
-{
+public class ConsulRecoveryManager {
     private static final Logger logger = LoggerFactory.getLogger(ConsulRecoveryManager.class);
     private static final ConsulConfig config =
             (ConsulConfig) Config.getInstance().getJsonObjectConfig(ConsulConstants.CONFIG_NAME, ConsulConfig.class);
     private static final AtomicBoolean shutdown = new AtomicBoolean(false);
 
+    private static final AtomicBoolean monitorThreadStarted = new AtomicBoolean(false);
+    private static final ConcurrentHashMap<String,Long> heartbeats = new ConcurrentHashMap<>();
+    private static final ConsulThreadMonitor consulThreadMonitor = new ConsulThreadMonitor(heartbeats);
+
     private boolean isRecoveryMode;
     private long recoveryAttempts = 0;
     private String serviceName;
 
-    public ConsulRecoveryManager(String serviceName) { this.serviceName = serviceName; }
+    public ConsulRecoveryManager(String serviceName) {
+        this.serviceName = serviceName;
+        startConsulThreadMonitor();
+    }
+
+    private static synchronized void startConsulThreadMonitor() {
+        if(monitorThreadStarted.get()) return;
+        monitorThreadStarted.set(true);
+        logger.debug("Starting Consul Thread Monitor...");
+        consulThreadMonitor.start();
+    }
 
     /**
      * Exit Consul connection recovery mode
@@ -53,6 +66,11 @@ public class ConsulRecoveryManager
         logger.error("System shutdown initiated - Consul connection could not be reestablished");
         shutdown.set(true);
         System.exit(1);
+    }
+
+    public void checkin() {
+        logger.debug("Service {} checking in", serviceName);
+        heartbeats.put(serviceName, System.currentTimeMillis());
     }
 
     public boolean isRecoveryMode() { return isRecoveryMode; }
