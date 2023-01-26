e70f304c6e1ce7df1390e8964256c7a2b60ef5db
Steve Hu <stevehu@gmail.com>
2023-01-26 12:51:32 -0500

    fixes #1591 update test-vault.sql to change value colume to val (#1592)

--- data-source/src/test/resources/scripts/test-vault.sql
+++ data-source/src/test/resources/scripts/test-vault.sql
@@ -1,16 +1,11 @@
-/*
-This is the defult vault database and you can create as many as you want for the
-same tokenization instance.
- */
 DROP TABLE IF EXISTS token_vault;
 
 CREATE TABLE token_vault (
-  id VARCHAR(1024) NOT NULL,
-  value VARCHAR(1024) NOT NULL,
-  PRIMARY KEY (id)
+  id VARCHAR(255) NOT NULL PRIMARY KEY,
+  val VARCHAR(255) NOT NULL
 );
 
-CREATE INDEX value_idx ON token_vault(value);
+CREATE INDEX value_idx ON token_vault(val);
 
-INSERT INTO token_vault(id, value) VALUES('4158281123', '4323927763');
-INSERT INTO token_vault(id, value) VALUES('2123765411126543', '5645337854346543');
+INSERT INTO token_vault(id, val) VALUES('4158281123', '4323927763');
+INSERT INTO token_vault(id, val) VALUES('2123765411126543', '5645337854346543');
