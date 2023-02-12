e6627e8ec8e2d364238d6226b9a8b09ced152ba0
miklish <michael.n.christoff@gmail.com>
2023-02-12 01:34:30 -0500

    2nd version

--- client/src/main/java/com/networknt/client/http/SimpleURIConnectionPool.java
+++ client/src/main/java/com/networknt/client/http/SimpleURIConnectionPool.java
@@ -1,27 +1,31 @@
 package com.networknt.client.http;
 
+import io.undertow.client.ClientConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.net.URI;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
-
 /***
     A connection pool for a single URI.
-    Connection pool contains 4 Sets of ConnectionHolders
+    Connection pool contains 4 Sets of ConnectionHolders:
 
-    1. All holders -- the complete set of holders
-    2. Borrowable holders -- holders that can be borrowed from
-    3. Borrowed holders -- holders that have borrowed tokens
-    4. Not Borrowed Expired holders -- expired holders that have no borrowed tokens -- these can be closed
+        1. All:                     the complete set of holders
+        2. Borrowable:              holders that can be borrowed from
+        3. Borrowed:                holders that have borrowed tokens
+        4. Not Borrowed Expired:    expired holders that have no borrowed tokens -- these can be closed
 */
 public class SimpleURIConnectionPool {
+    private static final Logger logger = LoggerFactory.getLogger(SimpleURIConnectionPool.class);
     private long EXPIRY_TIME;
     private int poolSize;
     private URI uri;
-    private Set<SimpleConnectionHolder> all = new HashSet<>();
-    private Set<SimpleConnectionHolder> borrowable = new HashSet<>();
-    private Set<SimpleConnectionHolder> borrowed = new HashSet<>();
-    private Set<SimpleConnectionHolder> notBorrowedExpired = new HashSet<>();
+    private final Set<SimpleConnectionHolder> all = new HashSet<>();
+    private final Set<SimpleConnectionHolder> borrowable = new HashSet<>();
+    private final Set<SimpleConnectionHolder> borrowed = new HashSet<>();
+    private final Set<SimpleConnectionHolder> notBorrowedExpired = new HashSet<>();
 
     private SimpleURIConnectionPool() {}
 
@@ -31,47 +35,131 @@ public class SimpleURIConnectionPool {
         this.poolSize = poolSize;
     }
 
-    public synchronized SimpleConnectionHolder.ConnectionToken borrow(long timeout, long createConnectionTimeout, boolean isHttp2) {
+    public synchronized SimpleConnectionHolder.ConnectionToken borrow(long createConnectionTimeout, boolean isHttp2)
+        throws RuntimeException
+    {
+        long now = System.currentTimeMillis();
         SimpleConnectionHolder holder = null;
-        readHolders();
+
+        readAllHolders(now);
 
         if(borrowable.size() > 0) {
             holder = borrowable.toArray(new SimpleConnectionHolder[0])[ThreadLocalRandom.current().nextInt(borrowable.size())];
+        } else {
+            if (borrowed.size() < poolSize) {
+                holder = new SimpleConnectionHolder(EXPIRY_TIME, createConnectionTimeout, isHttp2, uri);
+                all.add(holder);
+            } else
+                throw new RuntimeException("An attempt to exceed the connection pool's maximum size was made. Increase request.connectionPoolSize in client.yml");
         }
-        else if(borrowed.size() < poolSize)
-            holder = new SimpleConnectionHolder(timeout, createConnectionTimeout, isHttp2, uri);
 
-        SimpleConnectionHolder.ConnectionToken token = holder.borrow(timeout);
-        readHolder(holder);
+        SimpleConnectionHolder.ConnectionToken token = holder.borrow(createConnectionTimeout, now);
+        readSingleHolder(holder, now);
+
+        logger.debug(showConnections("borrow"));
+
         return token;
     }
 
     public synchronized void restore(SimpleConnectionHolder.ConnectionToken token) {
-        readHolders();
-        token.holder().restore(token);
-        readHolder(token.holder());
+        if(token == null)
+            return;
+
+        //SimpleConnectionHolder holder = null;
+        SimpleConnectionHolder holder = token.holder();
+        long now = System.currentTimeMillis();
+
+        readAllHolders(now);
+        holder.restore(token);
+        readSingleHolder(holder, now);
+
+        logger.debug(showConnections("restore"));
     }
 
-    private void readHolders() {
-        for(SimpleConnectionHolder holder: all) readHolder(holder);
+    /***
+     * This method reads a holder and updates the state of the SimpleURIConnectionPool based on the state of connection.
+     *
+     *
+     * @param holder
+     * @param now
+     */
+    private void readSingleHolder(SimpleConnectionHolder holder, long now) {
 
-        for(SimpleConnectionHolder holder: notBorrowedExpired) {
-            holder.close();
-            notBorrowedExpired.remove(holder);
+        if(holder.closed()) {
             all.remove(holder);
+            borrowable.remove(holder);
+            borrowed.remove(holder);
+            notBorrowedExpired.remove(holder);
+            return;
+        }
+
+        boolean isExpired =             holder.expired(now);
+        boolean isBorrowed =            holder.borrowed();
+        boolean isBorrowable =          holder.borrowable(now);
+        boolean isNotBorrowedExpired =  !isBorrowed && isExpired;
+
+        // check whether holder should be added or removed from these sets based on its current state
+        updateSet(borrowable, isBorrowable, holder);
+        updateSet(borrowed, isBorrowed, holder);
+        updateSet(notBorrowedExpired, isNotBorrowedExpired, holder);
+    }
+
+    /**
+     * A key method that actually closes connections
+     * It is guaranteed to run every time a transition method is called on SimpleURIConnectionPool
+     *
+     * @param now the current time in ms
+     */
+    private void readAllHolders(long now) {
+        // sweep all connections and update sets
+        for(SimpleConnectionHolder holder: all)
+            readSingleHolder(holder, now);
+
+        // close any connections found in a closeable set
+        for(SimpleConnectionHolder closeable: notBorrowedExpired) {
+            closeable.close(now);
+            notBorrowedExpired.remove(closeable);  // after a sweep
+            all.remove(closeable);
         }
     }
 
-    private void readHolder(SimpleConnectionHolder holder) {
-        updateSet(borrowable, holder.borrowable(), holder);
-        updateSet(borrowed, holder.borrowed(), holder);
-        updateSet(notBorrowedExpired, holder.closeable(), holder);
-    }
-
+    /***
+     * Takes a Set, a boolean, and a holder
+     * If the boolean is true, it will add the holder from the Set, otherwise, it will remove it from the Set
+     *
+     * @param set the set to potentially add or remove the holder from
+     * @param isMember if true, it will add holder to set, otherwise, it will remove holder from set
+     * @param holder
+     */
+    // TODO: Ensure this does not throw errors!
     private void updateSet(Set<SimpleConnectionHolder> set, boolean isMember, SimpleConnectionHolder holder) {
         if(isMember && !set.contains(holder))
             set.add(holder);
-        else if(!isMember && set.contains(holder))
+        else if(!isMember)
             set.remove(holder);
+    }
+
+    // for logging
+    public String showConnections(String transitionName) {
+        return "After " + transitionName + " - CONNECTIONS: " +
+                showConnections("BORROWABLE", borrowable) +
+                showConnections("BORROWED", borrowed) +
+                showConnections("NOT_BORROWED_EXPIRED", notBorrowedExpired);
+    }
+
+    public static String showConnections(String name, Set<SimpleConnectionHolder> set) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[").append(name).append(": ");
+        for(SimpleConnectionHolder holder: set)
+            sb.append(port(holder.connection())).append(" ");
+        sb.append("] ");
+        return sb.toString();
+    }
+    public static String port(ClientConnection connection) {
+        if(connection == null) return "NULL";
+        String url = connection.getLocalAddress().toString();
+        int semiColon = url.lastIndexOf(":");
+        if(semiColon == - 1) return "PORT?";
+        return url.substring(url.lastIndexOf(":")+1);
     }
 }
