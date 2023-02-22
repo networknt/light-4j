33c8b6c7a8b94b68839b34d18ec2adb482ee6a82
Steve Hu <stevehu@gmail.com>
2023-02-22 15:06:20 -0500

    fixes #1625 update the security.yml to add passThroughClaims (#1626)

--- security/src/test/java/com/networknt/security/SecurityConfigTest.java
+++ security/src/test/java/com/networknt/security/SecurityConfigTest.java
@@ -3,6 +3,8 @@ package com.networknt.security;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.security.Security;
+
 public class SecurityConfigTest {
     @Test
     public void testLoadDefaultConfig() {
@@ -18,4 +20,15 @@ public class SecurityConfigTest {
         Assert.assertEquals(2, config.getCertificate().size());
     }
 
+    @Test
+    public void testJsonPassThroughClaims() {
+        SecurityConfig config = SecurityConfig.load("security-json-claims");
+        Assert.assertEquals(2, config.getPassThroughClaims().size());
+    }
+
+    @Test
+    public void testYamlPassThroughClaims() {
+        SecurityConfig config = SecurityConfig.load("security-yaml-claims");
+        Assert.assertEquals(2, config.getPassThroughClaims().size());
+    }
 }
