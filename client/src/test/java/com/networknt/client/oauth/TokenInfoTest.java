47e82f7bade5132fa0cbed0c4f290b193a26cbf0
Steve Hu <stevehu@gmail.com>
2023-02-22 15:05:55 -0500

    fixes #1625 update the security.yml to add passThroughClaims

--- client/src/test/java/com/networknt/client/oauth/TokenInfoTest.java
+++ client/src/test/java/com/networknt/client/oauth/TokenInfoTest.java
@@ -2,7 +2,10 @@ package com.networknt.client.oauth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.networknt.config.JsonMapper;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.lang.reflect.Field;
 
 public class TokenInfoTest {
     /**
@@ -26,6 +29,18 @@ public class TokenInfoTest {
         ObjectMapper mapper = new ObjectMapper();
         TokenInfo tokenInfo = mapper.readValue(json, TokenInfo.class);
         System.out.println("tokenInfo = " + tokenInfo);
+    }
 
+    @Test
+    public void testPropertyGet() throws Exception {
+        String json = "{  \"active\": true,\"client_id\": \"bb8293f6-ceef-4e7a-90c8-1492e97df19f\",\"token_type\": \"refresh_token\",\"scope\": \"openid profile\",\"sub\": \"cn=odicuser,dc=example,dc=com\",\"exp\": \"86400\",\"iat\": \"1506513918\",\"iss\": \"https://wa.example.com\" }";
+        ObjectMapper mapper = new ObjectMapper();
+        TokenInfo object = mapper.readValue(json, TokenInfo.class);
+
+        Field field = object.getClass().getDeclaredField("clientId");
+        field.setAccessible(true);
+        Object value = field.get(object);
+        Assert.assertEquals("bb8293f6-ceef-4e7a-90c8-1492e97df19f", value);
+        System.out.println("value = " + value);
     }
 }
