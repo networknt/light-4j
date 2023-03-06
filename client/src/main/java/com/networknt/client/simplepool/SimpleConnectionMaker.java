7b8c67ecf1111076a6fe43ecf5dfa241373d9f5b
miklish <michael.n.christoff@gmail.com>
2023-03-06 17:23:44 -0500

    Added additional docs

--- client/src/main/java/com/networknt/client/simplepool/SimpleConnectionMaker.java
+++ client/src/main/java/com/networknt/client/simplepool/SimpleConnectionMaker.java
@@ -3,6 +3,11 @@ package com.networknt.client.simplepool;
 import java.net.URI;
 import java.util.Set;
 
+/***
+ * A factory that creates raw connections and wraps them in SimpleConnection objects.
+ * SimpleConnectionMakers are used by SimpleConnectionHolders to create connections.
+ *
+ */
 public interface SimpleConnectionMaker {
     public SimpleConnection makeConnection(long createConnectionTimeout, boolean isHttp2, final URI uri, final Set<SimpleConnection> allCreatedConnections);
     public SimpleConnection reuseConnection(long createConnectionTimeout, SimpleConnection connection) throws RuntimeException;
