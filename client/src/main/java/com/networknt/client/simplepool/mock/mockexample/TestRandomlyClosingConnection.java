fb59ee19115beb836df0e8eb7132bd7ca829da63
miklish <michael.n.christoff@gmail.com>
2023-03-08 23:08:18 -0500

    Move example mock tests to sub-package mockexample in simplepool.mock

---
+++ client/src/main/java/com/networknt/client/simplepool/mock/mockexample/TestRandomlyClosingConnection.java
@@ -0,0 +1,19 @@
+package com.networknt.client.simplepool.mock.mockexample;
+
+import com.networknt.client.simplepool.mock.TestRunner;
+
+public class TestRandomlyClosingConnection
+{
+    public static void main(String[] args) {
+        new TestRunner()
+            .setConnectionClass(MockRandomlyClosingConnection.class)
+            .setExpireTime(5)
+            .setCreateConnectionTimeout(5)
+            .setHttp2(true)
+            .setBorrowTime(5)
+            .setBorrowTimeJitter(5)
+            .setNumCallers(8)
+            .setTestLength(10*60)
+            .executeTest();
+    }
+}
