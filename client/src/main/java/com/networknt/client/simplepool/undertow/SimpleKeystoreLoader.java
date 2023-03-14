54a8f2e08c3eb33744a5c9c4fbbf35b97be9bd23
miklish <michael.n.christoff@gmail.com>
2023-03-14 05:00:48 -0400

    add license and attributions

--- client/src/main/java/com/networknt/client/simplepool/undertow/SimpleKeystoreLoader.java
+++ client/src/main/java/com/networknt/client/simplepool/undertow/SimpleKeystoreLoader.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author miklish Michael N. Christoff
+ * Originally from com.networknt.utility.TlsUtil by Balloon
+ */
 package com.networknt.client.simplepool.undertow;
 
 import com.networknt.config.Config;
@@ -7,7 +23,7 @@ import java.io.InputStream;
 import java.security.KeyStore;
 
 /**
- * Originally from com.networknt.utility.TlsUtil developed by Balloon
+ * Originally from com.networknt.utility.TlsUtil by Balloon
  */
 
 public class SimpleKeystoreLoader
