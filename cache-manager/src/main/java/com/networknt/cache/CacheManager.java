cf052b5777b7e021c852474ceb9d50f7ed6580c7
Steve Hu <stevehu@gmail.com>
2023-04-06 21:01:44 -0400

    fixes #1697 check cache configuration first before creating CacheManager instance (#1698)

--- cache-manager/src/main/java/com/networknt/cache/CacheManager.java
+++ cache-manager/src/main/java/com/networknt/cache/CacheManager.java
@@ -5,23 +5,28 @@ import com.networknt.service.SingletonServiceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 public interface CacheManager {
     Logger logger = LoggerFactory.getLogger(CacheManager.class);
     static CacheManager getInstance() {
-        CacheManager cacheManager = SingletonServiceFactory.getBean(CacheManager.class);
-        if (cacheManager == null) {
-            logger.warn("No cache manager configured in service.yml; default to CaffeineCacheManager");
-            cacheManager = new CaffeineCacheManager();
-            for(CacheItem cacheItem: CacheConfig.load().getCaches()) {
-                cacheManager.addCache(cacheItem.getCacheName(), cacheItem.getMaxSize(), cacheItem.getExpiryInMinutes());
+        List<CacheItem> caches = CacheConfig.load().getCaches();
+        if(caches != null && caches.size() > 0) {
+            CacheManager cacheManager = SingletonServiceFactory.getBean(CacheManager.class);
+            if (cacheManager == null) {
+                logger.warn("No cache manager configured in service.yml; default to CaffeineCacheManager");
+                cacheManager = new CaffeineCacheManager();
+                for(CacheItem cacheItem: CacheConfig.load().getCaches()) {
+                    cacheManager.addCache(cacheItem.getCacheName(), cacheItem.getMaxSize(), cacheItem.getExpiryInMinutes());
+                }
+                SingletonServiceFactory.setBean(CacheManager.class.getName(), cacheManager);
             }
-            SingletonServiceFactory.setBean(CacheManager.class.getName(), cacheManager);
+            return cacheManager;
+        } else {
+            return null;
         }
-        return cacheManager;
-
     }
+
     void addCache(String cacheName, long maxSize, long expiryInMinutes);
     Cache<Object, Object> getCache(String cacheName);
     void removeCache(String cacheName);
