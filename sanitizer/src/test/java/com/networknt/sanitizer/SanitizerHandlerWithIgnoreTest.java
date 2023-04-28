3f50d356badc9444b8ff094f65872e84f24819fb
Steve Hu <stevehu@gmail.com>
2023-04-28 14:26:16 -0400

    fixes #1747 add test cases for header encode and ignore lists (#1748)

--- sanitizer/src/test/java/com/networknt/sanitizer/SanitizerHandlerWithIgnoreTest.java
+++ sanitizer/src/test/java/com/networknt/sanitizer/SanitizerHandlerWithIgnoreTest.java
@@ -10,6 +10,7 @@ import io.undertow.client.ClientConnection;
 import io.undertow.client.ClientRequest;
 import io.undertow.client.ClientResponse;
 import io.undertow.util.Headers;
+import io.undertow.util.HttpString;
 import io.undertow.util.Methods;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -29,14 +30,14 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class SanitizerHandlerWithIgnoreTest {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SanitizerHandlerWithIgnoreTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(SanitizerHandlerWithIgnoreTest.class);
 
     private static Undertow server = null;
 
     @BeforeClass
     public static void setUp() {
         if(server == null) {
-            LOGGER.info("starting server");
+            logger.info("starting server");
             server = ServerBuilder.newServer().withConfigName("sanitizer_with_ignore").build();
             server.start();
         }
@@ -51,7 +52,7 @@ public class SanitizerHandlerWithIgnoreTest {
 
             }
             server.stop();
-            LOGGER.info("The server is stopped.");
+            logger.info("The server is stopped.");
         }
     }
 
@@ -83,7 +84,7 @@ public class SanitizerHandlerWithIgnoreTest {
 
             latch.await(10, TimeUnit.SECONDS);
         } catch (Exception e) {
-            LOGGER.error("IOException: ", e);
+            logger.error("IOException: ", e);
             throw new ClientException(e);
         } finally {
             IoUtils.safeClose(connection);
@@ -98,4 +99,104 @@ public class SanitizerHandlerWithIgnoreTest {
             Assert.assertEquals("<script>alert('test')</script>", map.get("key"));
         }
     }
+
+    /**
+     * Since the param header is ignored, the value is not encoded,  and it should be the same as the original value.
+     * @throws Exception exception
+     */
+    @Test
+    public void testGetHeader() throws Exception {
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
+        try {
+            connection = client.connect(new URI("http://localhost:7080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        try {
+            ClientRequest request = new ClientRequest().setPath("/header").setMethod(Methods.GET);
+            request.getRequestHeaders().put(Headers.HOST, "localhost");
+            request.getRequestHeaders().put(new HttpString("param"), "<script>alert('header test')</script>");
+            connection.sendRequest(request, client.createClientCallback(reference, latch));
+            latch.await();
+        } catch (Exception e) {
+            logger.error("Exception: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        int statusCode = reference.get().getResponseCode();
+        String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+        Assert.assertEquals(200, statusCode);
+        Assert.assertTrue(body.contains("<script>alert('header test')</script>"));
+    }
+
+    /**
+     * Since the param2 header is encoded, the value is escaped and it should be the same as the original value.
+     * @throws Exception exception
+     */
+    @Test
+    public void testGetEncodedHeader() throws Exception {
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
+        try {
+            connection = client.connect(new URI("http://localhost:7080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        try {
+            ClientRequest request = new ClientRequest().setPath("/header").setMethod(Methods.GET);
+            request.getRequestHeaders().put(Headers.HOST, "localhost");
+            request.getRequestHeaders().put(new HttpString("param2"), "<script>alert('header param2')</script>");
+            connection.sendRequest(request, client.createClientCallback(reference, latch));
+            latch.await();
+        } catch (Exception e) {
+            logger.error("Exception: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        int statusCode = reference.get().getResponseCode();
+        String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+        Assert.assertEquals(200, statusCode);
+        Assert.assertTrue(body.contains("<script>alert(\\'header param2\\')</script>"));
+    }
+
+    /**
+     * Since the param3 header is not in encoded list, the value is not encoded, and it should be the same as the original value.
+     * @throws Exception exception
+     */
+    @Test
+    public void testGetNotEncodedHeaderWithEncodedNotEmpty() throws Exception {
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
+        try {
+            connection = client.connect(new URI("http://localhost:7080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        try {
+            ClientRequest request = new ClientRequest().setPath("/header").setMethod(Methods.GET);
+            request.getRequestHeaders().put(Headers.HOST, "localhost");
+            request.getRequestHeaders().put(new HttpString("param3"), "<script>alert('header param3')</script>");
+            connection.sendRequest(request, client.createClientCallback(reference, latch));
+            latch.await();
+        } catch (Exception e) {
+            logger.error("Exception: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        int statusCode = reference.get().getResponseCode();
+        String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+        Assert.assertEquals(200, statusCode);
+        Assert.assertTrue(body.contains("<script>alert('header param3')</script>"));
+    }
+
 }
