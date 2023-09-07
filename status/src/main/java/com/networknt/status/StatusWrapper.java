b67bf8e34b211f239126d3d03a21e53f3706ece9
Steve Hu <stevehu@gmail.com>
2023-09-07 12:46:13 -0400

    fixes #1875 remove dependency to Undertow in status module

--- status/src/main/java/com/networknt/status/StatusWrapper.java
+++ status/src/main/java/com/networknt/status/StatusWrapper.java
@@ -1,24 +1,24 @@
 package com.networknt.status;
 
-import io.undertow.server.HttpServerExchange;
-
 /**
  * Interface to allow custom Status and then inject the customized status into framework through SingletonFactory
  * <p>
  * Implement the interface and configure in service.yml
  * - com.networknt.status.StatusWrapper
- *  - custom implementation
+ *   - custom implementation class package
  *
  * @author Jiachen Sun
  */
 
 public interface StatusWrapper {
     /**
-     * Encapsulate the default status of the framework as a custom status
+     * Encapsulate the default status of the framework as a custom status. If you want to add information from
+     * the HttpServerExchange, you can use the exchange parameter. However, it is passed as an Object, so you
+     * need to cast it to the actual type in your implementation class.
      *
      * @param status   The status to be wrapped
      * @param exchange The source of custom info used to wrap status
      * @return the custom status
      */
-    Status wrap(Status status, HttpServerExchange exchange);
+    Status wrap(Status status, Object exchange);
 }
