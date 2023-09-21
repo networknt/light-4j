a76367124f3ca9847b2969b1a7e59bc1cab350fa
Steve Hu <stevehu@gmail.com>
2023-09-21 16:08:40 -0400

    fixes #1891 Update the pom.xml and audit test cases to build with JDK 17

--- audit/src/test/java/com/networknt/audit/AuditConfigTest.java
+++ audit/src/test/java/com/networknt/audit/AuditConfigTest.java
@@ -20,9 +20,7 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
 public class AuditConfigTest {
 
