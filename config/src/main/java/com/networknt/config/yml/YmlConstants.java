d37314d70c6f5118e5eef3e86f96b1e1f0539fbe
ForwardKeys <54978131+ForwardKeys@users.noreply.github.com>
2023-10-24 19:10:22 +0200

    Fixes #1908 (#1930)
    CRYPT regexp in YmlConstants has been adapted only to allow AES alike passwords, rendering all previous passwords no longer valid. This regex also invalidates the possibility of using customised Decryptors.

--- config/src/main/java/com/networknt/config/yml/YmlConstants.java
+++ config/src/main/java/com/networknt/config/yml/YmlConstants.java
@@ -6,6 +6,6 @@ import org.yaml.snakeyaml.nodes.Tag;
 
 public class YmlConstants {
 	public static final Tag CRYPT_TAG = new Tag(Tag.PREFIX + "crypt");
-	public static final Pattern CRYPT_PATTERN = Pattern.compile("CRYPT:[a-zA-Z0-9:=/+]+");
+	public static final Pattern CRYPT_PATTERN = Pattern.compile("^CRYPT:.*$");
 	public static final String CRYPT_FIRST = "C"; 
 }
