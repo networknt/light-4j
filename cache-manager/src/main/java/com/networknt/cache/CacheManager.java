603209c6cbf4b918ae8a2bcd1cc9ffd04eef56a3
Steve Hu <stevehu@gmail.com>
2023-10-31 11:42:01 -0400

    fixes #1942 split the cache-manager to move caffeine cache to a separate module (#1943)

--- cache-manager/src/main/java/com/networknt/cache/CacheManager.java
+++ cache-manager/src/main/java/com/networknt/cache/CacheManager.java
@@ -1,33 +1,40 @@
 package com.networknt.cache;
 
-import com.github.benmanes.caffeine.cache.Cache;
 import com.networknt.service.SingletonServiceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
+/**
+ * CacheManager is a singleton class that is used to manage all the caches in the system. The underline implementation
+ * can be anything as long as it implements the CacheManager interface. The default implementation is CaffeineCacheManager
+ * Please note that the CacheManager doesn't have any reference to the underlying cache implementation.
+ *
+ * @author Steve Hu
+ */
 public interface CacheManager {
     Logger logger = LoggerFactory.getLogger(CacheManager.class);
     static CacheManager getInstance() {
         List<CacheItem> caches = CacheConfig.load().getCaches();
-        if(caches != null && caches.size() > 0) {
+        if(caches != null && !caches.isEmpty()) {
             CacheManager cacheManager = SingletonServiceFactory.getBean(CacheManager.class);
-            if (cacheManager == null) {
-                logger.warn("No cache manager configured in service.yml; default to CaffeineCacheManager");
-                cacheManager = new CaffeineCacheManager();
-                for(CacheItem cacheItem: CacheConfig.load().getCaches()) {
+            if(cacheManager != null) {
+                for(CacheItem cacheItem: caches) {
                     cacheManager.addCache(cacheItem.getCacheName(), cacheItem.getMaxSize(), cacheItem.getExpiryInMinutes());
                 }
-                SingletonServiceFactory.setBean(CacheManager.class.getName(), cacheManager);
+            } else {
+                logger.error("CacheManager implementation is not found in the service.yml");
             }
             return cacheManager;
         } else {
+            logger.error("No cache is configured in cache.yml");
             return null;
         }
     }
 
     void addCache(String cacheName, long maxSize, long expiryInMinutes);
-    Cache<Object, Object> getCache(String cacheName);
+    void put(String cacheName, String key, Object value);
+    Object get(String cacheName, String key);
     void removeCache(String cacheName);
 }
--- caffeine-cache/src/test/java/com/networknt/cache/DefaultCacheManagerTest.java
+++ caffeine-cache/src/test/java/com/networknt/cache/DefaultCacheManagerTest.java
@@ -1,6 +1,5 @@
 package com.networknt.cache;
 
-import com.github.benmanes.caffeine.cache.Cache;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,19 +8,20 @@ public class DefaultCacheManagerTest {
     public void testDefaultCacheManager() {
         CacheManager cacheManager = CacheManager.getInstance();
         cacheManager.addCache("test", 100, 10);
-        Cache<Object, Object> cache = cacheManager.getCache("test");
-        cache.put("key", "value");
-        Assert.assertEquals("value", cache.getIfPresent("key"));
+        cacheManager.put("test", "key", "value");
+        Object value = cacheManager.get("test", "key");
+        Assert.assertEquals("value", value);
         cacheManager.removeCache("test");
-        Assert.assertNull(cache.getIfPresent("key"));
+        value = cacheManager.get("test", "key");
+        Assert.assertNull(value);
     }
 
     @Test
     public void testConfiguredCacheManager() {
         CacheManager cacheManager = CacheManager.getInstance();
         // there should be two caches in the cache manager.
-        Cache<Object, Object> cache1 = cacheManager.getCache("cache1");
-        cache1.put("key", "value");
-        Assert.assertEquals("value", cache1.getIfPresent("key"));
+        cacheManager.put("cache1", "key", "value");
+        Object value = cacheManager.get("cache1", "key");
+        Assert.assertEquals("value", value);
     }
 }
