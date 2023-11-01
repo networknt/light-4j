89d0017058df376ffcfddf8746551192beb3e23a
miklish <michael.n.christoff@gmail.com>
2023-11-01 13:02:42 -0400

    Update com.networknt.client.Http2ClientPoolTest.testResponseTime() and com.networknt.client.Http2ClientTest.testResponseTime() unit tests to succeed on response times less than 1ms (#1951)
    Co-authored-by: Michael Christoff <mike.christoff@cibc.com>

--- client/src/test/java/com/networknt/client/Http2ClientPoolTest.java
+++ client/src/test/java/com/networknt/client/Http2ClientPoolTest.java
@@ -286,7 +286,7 @@ public class Http2ClientPoolTest {
             final AsyncResult<AsyncResponse> ar = reference.get();
             if(ar.succeeded()) {
                 Assert.assertEquals(message, ar.result().getResponseBody());
-                Assert.assertTrue(ar.result().getResponseTime() > 0);
+                Assert.assertTrue(ar.result().getResponseTime() >= 0);
                 System.out.println("responseTime = " + ar.result().getResponseTime());
             } else {
                 ar.cause().printStackTrace();
