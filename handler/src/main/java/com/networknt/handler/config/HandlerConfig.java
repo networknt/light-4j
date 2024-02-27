cbb62b2c1b35f6b40f49c1f3e7437c28e4268c22
Steve Hu <stevehu@gmail.com>
2024-02-26 22:11:16 -0500

    fixes #2159 HandlerConfig supports additionalHandlers, additionalChains and additionalPaths (#2160)

--- handler/src/main/java/com/networknt/handler/config/HandlerConfig.java
+++ handler/src/main/java/com/networknt/handler/config/HandlerConfig.java
@@ -34,8 +34,11 @@ public class HandlerConfig {
     private static final Logger logger = LoggerFactory.getLogger(HandlerConfig.class);
     private static final String ENABLED = "enabled";
     private static final String HANDLERS = "handlers";
+    private static final String ADDITIONAL_HANDLERS = "additionalHandlers";
     private static final String CHAINS = "chains";
+    private static final String ADDITIONAL_CHAINS = "additionalChains";
     private static final String PATHS = "paths";
+    private static final String ADDITIONAL_PATHS = "additionalPaths";
     private static final String DEFAULT_HANDLERS = "defaultHandlers";
     private static final String AUDIT_ON_ERROR = "auditOnError";
     private static final String AUDIT_STACK_TRACE = "auditStackTrace";
@@ -199,6 +202,31 @@ public class HandlerConfig {
             } else {
                 throw new ConfigException("handlers must be a string or a list of strings.");
             }
+            // add additional handlers to the handlers if exist
+            if (mappedConfig.get(ADDITIONAL_HANDLERS) != null) {
+                object = mappedConfig.get(ADDITIONAL_HANDLERS);
+                if(object instanceof String) {
+                    String s = (String)object;
+                    s = s.trim();
+                    if(logger.isTraceEnabled()) logger.trace("s = " + s);
+                    if(s.startsWith("[")) {
+                        // json format
+                        try {
+                            handlers.addAll(Config.getInstance().getMapper().readValue(s, new TypeReference<>() {}));
+                        } catch (Exception e) {
+                            throw new ConfigException("could not parse the additionalHandlers json with a list of strings.");
+                        }
+                    } else {
+                        // comma separated
+                        handlers.addAll(Arrays.asList(s.split("\\s*,\\s*")));
+                    }
+                } else if (object instanceof List) {
+                    List<String> list = (List)object;
+                    handlers.addAll(list);
+                } else {
+                    throw new ConfigException("additionalHandlers must be a string or a list of strings.");
+                }
+            }
         }
         if (mappedConfig != null && mappedConfig.get(DEFAULT_HANDLERS) != null) {
             Object object = mappedConfig.get(DEFAULT_HANDLERS);
@@ -255,6 +283,38 @@ public class HandlerConfig {
                 }
             } else {
                 throw new ConfigException("paths must be a list of string object map.");
+            }
+            // add additional paths to the paths if exist
+            if (mappedConfig.get(ADDITIONAL_PATHS) != null) {
+                object = mappedConfig.get(ADDITIONAL_PATHS);
+                if(object instanceof String) {
+                    String s = (String)object;
+                    s = s.trim();
+                    if(logger.isTraceEnabled()) logger.trace("s = " + s);
+                    if(s.startsWith("[")) {
+                        // json format
+                        try {
+                            paths.addAll(Config.getInstance().getMapper().readValue(s, new TypeReference<List<PathChain>>() {}));
+                        } catch (Exception e) {
+                            throw new ConfigException("could not parse the additionalPaths json with a list of string and object.");
+                        }
+                    } else {
+                        throw new ConfigException("additionalPaths must be a list of string object map.");
+                    }
+                } else if (object instanceof List) {
+                    // the object is a list of map, we need convert it to PathPrefixAuth object.
+                    List<Map<String, Object>> values = (List<Map<String, Object>>)object;
+                    for(Map<String, Object> value: values) {
+                        PathChain pathChain = new PathChain();
+                        pathChain.setPath((String)value.get(PATH));
+                        pathChain.setSource((String)value.get(SOURCE));
+                        pathChain.setMethod((String)value.get(METHOD));
+                        pathChain.setExec((List<String>)value.get(EXEC));
+                        paths.add(pathChain);
+                    }
+                } else {
+                    throw new ConfigException("additionalPaths must be a list of string object map.");
+                }
             }
         }
 
@@ -314,7 +374,60 @@ public class HandlerConfig {
                 } else {
                     throw new ConfigException("chains must be a string object map.");
                 }
+                // add additional chains to the chains if exist
+                if (mappedConfig.get(ADDITIONAL_CHAINS) != null) {
+                    chainsObject = mappedConfig.get(ADDITIONAL_CHAINS);
+                    if(chainsObject instanceof String) {
+                        String s = (String)chainsObject;
+                        s = s.trim();
+                        if(logger.isTraceEnabled()) logger.trace("s = " + s);
+                        if(s.startsWith("{")) {
+                            // json format
+                            try {
+                                chains.putAll(Config.getInstance().getMapper().readValue(s, new TypeReference<Map<String, List<String>>>() {}));
+                            } catch (Exception e) {
+                                throw new ConfigException("could not parse the additionalChains json with a map of string and list of strings.");
+                            }
+                        } else {
+                            throw new ConfigException("additionalChains must be a string object map.");
+                        }
+                    } else if (chainsObject instanceof Map) {
+                        Map<String, Object> map = (Map)chainsObject;
+                        for (Map.Entry<String, Object> entry : map.entrySet()) {
+                            String key = entry.getKey();
+                            Object value = entry.getValue();
+                            if (value instanceof String) {
+                                // convert to list of strings
+                                List<String> list;
+                                String s = (String) value;
+                                s = s.trim();
+                                if (logger.isTraceEnabled()) logger.trace("s = " + s);
+                                if (s.startsWith("[")) {
+                                    // json format
+                                    try {
+                                        list = Config.getInstance().getMapper().readValue(s, new TypeReference<List<String>>() {
+                                        });
+                                    } catch (Exception e) {
+                                        throw new ConfigException("could not parse the additionalChains json with a map of string and list of strings.");
+                                    }
+                                } else {
+                                    // comma separated
+                                    list = Arrays.asList(s.split("\\s*,\\s*"));
+                                }
+                                chains.put(key, list);
+                            } else if (value instanceof List) {
+                                // do nothing
+                                chains.put(key, (List) value);
+                            } else {
+                                throw new ConfigException("additionalChains must be a string object map.");
+                            }
+                        }
+                    } else {
+                        throw new ConfigException("additionalChains must be a string object map.");
+                    }
+                }
             }
+
         }
     }
 }
