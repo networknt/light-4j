0e74bc5eb36fda0c9eca70791f2f203deae4b3c0
Michael Christoff <mike.christoff@cibc.com>
2024-03-14 12:51:17 -0400

    correctly classify connection-creation timeouts by checking all future statuses (only WAITING means a timeout). Also update to use new custom exceptions

--- client/src/main/java/com/networknt/client/simplepool/undertow/SimpleUndertowConnectionMaker.java
+++ client/src/main/java/com/networknt/client/simplepool/undertow/SimpleUndertowConnectionMaker.java
@@ -22,6 +22,7 @@ package com.networknt.client.simplepool.undertow;
 import com.networknt.client.ClientConfig;
 import com.networknt.client.simplepool.SimpleConnection;
 import com.networknt.client.simplepool.SimpleConnectionMaker;
+import com.networknt.client.simplepool.exceptions.*;
 import io.undertow.Undertow;
 import io.undertow.UndertowOptions;
 import io.undertow.client.ClientCallback;
@@ -88,7 +89,7 @@ public class SimpleUndertowConnectionMaker implements SimpleConnectionMaker
 
             @Override
             public void failed(IOException e) {
-                if(logger.isDebugEnabled()) logger.debug("Failed to establish new connection for uri: {}", uri);
+                logger.error("Failed to establish new connection for uri {}: {}", uri, exceptionDetails(e));
                 result.setException(e);
             }
         };
@@ -105,25 +106,34 @@ public class SimpleUndertowConnectionMaker implements SimpleConnectionMaker
     /***
      * Never returns null
      *
-     * @param timeoutSeconds
-     * @param future
-     * @return
+     * @param timeoutSeconds connection timeout in seconds
+     * @param future contains future response containing new connection
+     * @return the new Undertow connection wrapped in a SimpleConnection
+     * @throws RuntimeException if connection fails
      */
-    private static SimpleConnection safeConnect(long timeoutSeconds, IoFuture<SimpleConnection> future)
+    private static SimpleConnection safeConnect(long timeoutSeconds, IoFuture<SimpleConnection> future) throws RuntimeException
     {
+        switch(future.await(timeoutSeconds, TimeUnit.SECONDS)) {
+            case DONE:
+                break;
+            case WAITING:
+                throw new SimplePoolConnectionFailureException("Connection establishment timed out after " + timeoutSeconds + " second(s)");
+            case FAILED:
+                Exception e = future.getException();
+                throw new SimplePoolConnectionFailureException("Connection establishment failed: " + exceptionDetails(e), e);
+            default:
+                throw new SimplePoolConnectionFailureException("Connection establishment failed");
+        }
+
         SimpleConnection connection = null;
-
-        if(future.await(timeoutSeconds, TimeUnit.SECONDS) != org.xnio.IoFuture.Status.DONE)
-            throw new RuntimeException("Connection establishment timed out");
-
         try {
             connection = future.get();
         } catch (IOException e) {
-            throw new RuntimeException("Connection establishment generated I/O exception", e);
+            throw new SimplePoolConnectionFailureException("Connection establishment generated I/O exception: " + exceptionDetails(e), e);
         }
 
         if(connection == null)
-            throw new RuntimeException("Connection establishment failed (null) - Full connection terminated");
+            throw new SimplePoolConnectionFailureException("Connection establishment failed (null) - Full connection terminated");
 
         return connection;
     }
@@ -194,6 +204,19 @@ public class SimpleUndertowConnectionMaker implements SimpleConnectionMaker
             }
         }
         return SSL.get();
+    }
+
+    /***
+     * Handles empty Exception messages for printing in logs (to avoid having "null" in logs for empty Exception messages)
+     *
+     * @param e Exception to look for a detail message in
+     * @return the Exception message, or "" if the Exception does not contain a message
+     */
+    public static String exceptionDetails(Exception e) {
+        if(e == null || e.getMessage() == null)
+            return "";
+        else
+            return e.getMessage();
     }
 
     public static String port(ClientConnection connection) {
