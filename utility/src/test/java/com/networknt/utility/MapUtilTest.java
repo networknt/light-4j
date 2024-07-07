d8e64edfe2ed30e61a8c07fca6909be0553ce73d
Steve Hu <stevehu@gmail.com>
2024-07-07 12:18:03 -0400

    fixes #2277 move MapUtil to light-4j utility module

---
+++ utility/src/test/java/com/networknt/utility/MapUtilTest.java
@@ -0,0 +1,26 @@
+package com.networknt.utility;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static com.networknt.utility.MapUtil.getValueIgnoreCase;
+
+public class MapUtilTest {
+    @Test
+    public void testGetValueIgnoreCase() {
+        Map<String, String> hashMap = new HashMap<>();
+        hashMap.put("Key1", "Value1");
+        hashMap.put("Key2", "Value2");
+
+        // Get value from HashMap with case-insensitive key lookup
+        String key = "key1";
+        Optional<String> value = getValueIgnoreCase(hashMap, key);
+        Assert.assertTrue(value.isPresent());
+    }
+
+}
