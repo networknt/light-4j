a8ebbcddbe22a238c301161cc5b297656eb91f6b
Steve Hu <stevehu@gmail.com>
2024-11-05 18:19:27 -0500

    fixes #2372 Add a test case to decrypt secret from the config to clear text (#2374)

--- decryptor/src/test/java/com/networknt/decrypt/AESDecryptorTest.java
+++ decryptor/src/test/java/com/networknt/decrypt/AESDecryptorTest.java
@@ -26,4 +26,17 @@ public class AESDecryptorTest {
         }
 
     }
+
+    /**
+     * A test case that can be used as a utility to decrypt encrypted value based on the master secret or password.
+     * The master secret will be loaded from the environment variable in this test case so that users master secret
+     * won't be revealed accidentally.
+     */
+    @Test
+    public void testDecryptWithEnv() {
+        String secretText = "CRYPT:ebfab3ef4261185776a026acf72d24ee:5eb239d133b840503eedf9b7de490341";
+        AutoAESSaltDecryptor decryptor = new AutoAESSaltDecryptor();
+        String clearText = decryptor.decrypt(secretText);
+        System.out.println("clearText = " + clearText);
+    }
 }
