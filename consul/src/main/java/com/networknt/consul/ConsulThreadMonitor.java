d4b497c8a1a270c18e79a43dbcc91639682a48d3
Steve Hu <stevehu@gmail.com>
2024-11-18 14:36:10 -0500

    fixes #2382 sync consul code from 1.6.x and add tag (#2383)

--- consul/src/main/java/com/networknt/consul/ConsulThreadMonitor.java
+++ consul/src/main/java/com/networknt/consul/ConsulThreadMonitor.java
@@ -3,7 +3,6 @@ package com.networknt.consul;
 import com.networknt.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
