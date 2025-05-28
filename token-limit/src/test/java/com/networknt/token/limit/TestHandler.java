d60ad626515add510686cfec57923ebdfc7ae455
Steve Hu <stevehu@gmail.com>
2025-05-28 12:12:06 -0400

    Sync (#2506)
    * Support for X_FORWARDED_FOR header to obtain client IP Address
    
    * fix for empty scope NPE and unit test cases improvement
    
    * - Added support for CachedResponseEntity with creation Timestamp in ms
     - Sync'ed cached response "expire_in" field before return to caller
     - Improved unit test cases to validate time drift by 1sec while token is unmodified
    
    * comment typo
    
    * conflict merge
    
    ---------
    
    Co-authored-by: mr78 <Diogo.Fekete@sunlife.com>
    Co-authored-by: GitHub Sync <github.sync@sunlife.com>

--- token-limit/src/test/java/com/networknt/token/limit/TestHandler.java
+++ token-limit/src/test/java/com/networknt/token/limit/TestHandler.java
@@ -6,13 +6,16 @@ import com.networknt.status.HttpStatus;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 
+import java.util.UUID;
+
 public class TestHandler implements LightHttpHandler {
     public static int counter = 0;
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
         exchange.getResponseHeaders().add(Headers.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
         exchange.setStatusCode(HttpStatus.OK.value());
-        String s = "{\"accessToken\":\"abc\",\"counter\": %d}";
+        UUID uuid = UUID.randomUUID();
+        String s = "{\"token_type\":\"bearer\",\"expire_in\":\"600\",\"access_token\":\"" + uuid.toString() + "\",\"counter\": %d}";
         s = String.format(s, counter++);
         exchange.getResponseSender().send(s);
     }
