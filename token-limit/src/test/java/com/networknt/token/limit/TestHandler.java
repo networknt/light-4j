2207e598096805525dd98a9c442c682d38724547
DiogoFKT <100703106+DiogoFKT@users.noreply.github.com>
2025-06-02 12:04:43 -0400

    Fix typo by exposing as configuration (#2508)
    * - Exposed expire key;
    - Logic to skip expire value update if empty;
    - Update unit tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- token-limit/src/test/java/com/networknt/token/limit/TestHandler.java
+++ token-limit/src/test/java/com/networknt/token/limit/TestHandler.java
@@ -15,7 +15,7 @@ public class TestHandler implements LightHttpHandler {
         exchange.getResponseHeaders().add(Headers.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
         exchange.setStatusCode(HttpStatus.OK.value());
         UUID uuid = UUID.randomUUID();
-        String s = "{\"token_type\":\"bearer\",\"expire_in\":\"600\",\"access_token\":\"" + uuid.toString() + "\",\"counter\": %d}";
+        String s = "{\"token_type\":\"bearer\",\"expires_in\":\"600\",\"access_token\":\"" + uuid.toString() + "\",\"counter\": %d}";
         s = String.format(s, counter++);
         exchange.getResponseSender().send(s);
     }
